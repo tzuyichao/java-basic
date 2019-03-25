5b2c5a0f4f19b476faf74bdcb1d81d4d37ed79bd
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-03-25 20:52:20 +0800

    remove unused import

--- java-basic/src/main/java/concurrent/ParallelMain2.java
+++ java-basic/src/main/java/concurrent/ParallelMain2.java
@@ -7,7 +7,6 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.stream.Collectors;
