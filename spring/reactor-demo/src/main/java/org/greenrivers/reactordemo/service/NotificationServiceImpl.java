b52e4af064e2fef5220c6e2c540b03e867446656
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-05-08 14:01:00 +0800

    refactor file name

--- spring/reactor-demo/src/main/java/org/greenrivers/reactordemo/service/NotificationServiceImpl.java
+++ spring/reactor-demo/src/main/java/org/greenrivers/reactordemo/service/NotificationServiceImpl.java
@@ -4,7 +4,7 @@ import org.greenrivers.reactordemo.model.NotificationData;
 import org.springframework.stereotype.Service;
 
 @Service
-public class NitificationServiceImpl implements NotificationService {
+public class NotificationServiceImpl implements NotificationService {
     @Override
     public void initiateNotification(NotificationData notificationData) throws InterruptedException {
         System.out.println("Notification service started for Notification Id " + notificationData.getId());
