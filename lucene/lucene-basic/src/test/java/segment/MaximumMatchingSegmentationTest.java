e035cce756b2058feec20f5178b5bee92ae35f58
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-05-21 14:16:36 +0800

    refactor name

--- lucene/lucene-basic/src/test/java/segment/MaximumMatchingSegmentationTest.java
+++ lucene/lucene-basic/src/test/java/segment/MaximumMatchingSegmentationTest.java
@@ -11,65 +11,65 @@ public class MaximumMatchingSegmentationTest {
 
     @Test
     public void test_stopWords() {
-        ForwardMaximumMatchingSegmentation segmenter = new ForwardMaximumMatchingSegmentation();
-        for(String stopWord : segmenter.getStopWords()) {
+        ForwardMaximumMatchingSegmentation segmentation = new ForwardMaximumMatchingSegmentation();
+        for(String stopWord : segmentation.getStopWords()) {
             System.out.println(stopWord);
         }
     }
 
     @Test
     public void test_dump_dictionary() {
-        ForwardMaximumMatchingSegmentation segmenter = new ForwardMaximumMatchingSegmentation();
-//        segmenter.getDictionary().forEach((term, detail) -> {
+        ForwardMaximumMatchingSegmentation segmentation = new ForwardMaximumMatchingSegmentation();
+//        segmentation.getDictionary().forEach((term, detail) -> {
 //            System.out.println(detail);
 //        });
-        System.out.println(segmenter.getDictionary().keySet().stream().max(Comparator.comparingInt(String::length)).get().length());
+        System.out.println(segmentation.getDictionary().keySet().stream().max(Comparator.comparingInt(String::length)).get().length());
     }
 
     @Test
     public void test_process() {
-        ForwardMaximumMatchingSegmentation segmenter = new ForwardMaximumMatchingSegmentation();
-        List<SegmentToken> tokens = segmenter.process("我们在野生动物园玩");
+        ForwardMaximumMatchingSegmentation segmentation = new ForwardMaximumMatchingSegmentation();
+        List<SegmentToken> tokens = segmentation.process("我们在野生动物园玩");
         assertNotNull(tokens);
         System.out.println(tokens);
     }
 
     @Test
     public void test_process_long_sentence() {
-        ForwardMaximumMatchingSegmentation segmenter = new ForwardMaximumMatchingSegmentation();
-        List<SegmentToken> tokens = segmenter.process("Akka是一种高度可扩展的软件，这不仅仅表现在性能方面，也表现在它所适用的应用的大小。");
+        ForwardMaximumMatchingSegmentation segmentation = new ForwardMaximumMatchingSegmentation();
+        List<SegmentToken> tokens = segmentation.process("Akka是一种高度可扩展的软件，这不仅仅表现在性能方面，也表现在它所适用的应用的大小。");
         assertNotNull(tokens);
         System.out.println(tokens);
     }
 
     @Test
     public void test_rmm_process() {
-        ReverseDirectionMaximumMatchingSegmentation segmenter = new ReverseDirectionMaximumMatchingSegmentation();
-        List<SegmentToken> tokens = segmenter.process("我们在野生动物园玩");
+        ReverseDirectionMaximumMatchingSegmentation segmentation = new ReverseDirectionMaximumMatchingSegmentation();
+        List<SegmentToken> tokens = segmentation.process("我们在野生动物园玩");
         assertNotNull(tokens);
         System.out.println(tokens);
     }
 
     @Test
     public void test_rmm_process_long_sentence() {
-        ReverseDirectionMaximumMatchingSegmentation segmenter = new ReverseDirectionMaximumMatchingSegmentation();
-        List<SegmentToken> tokens = segmenter.process("Akka是一种高度可扩展的软件，这不仅仅表现在性能方面，也表现在它所适用的应用的大小。");
+        ReverseDirectionMaximumMatchingSegmentation segmentation = new ReverseDirectionMaximumMatchingSegmentation();
+        List<SegmentToken> tokens = segmentation.process("Akka是一种高度可扩展的软件，这不仅仅表现在性能方面，也表现在它所适用的应用的大小。");
         assertNotNull(tokens);
         System.out.println(tokens);
     }
 
     @Test
     public void test_rmm_process2() {
-        MaximumMatchingSegmentation segmenter = new ReverseDirectionMaximumMatchingSegmentation();
-        List<SegmentToken> tokens = segmenter.process("研究生命的起源");
+        MaximumMatchingSegmentation segmentation = new ReverseDirectionMaximumMatchingSegmentation();
+        List<SegmentToken> tokens = segmentation.process("研究生命的起源");
         assertNotNull(tokens);
         System.out.println(tokens);
     }
 
     @Test
     public void test_process2() {
-        MaximumMatchingSegmentation segmenter = new ForwardMaximumMatchingSegmentation();
-        List<SegmentToken> tokens = segmenter.process("研究生命的起源");
+        MaximumMatchingSegmentation segmentation = new ForwardMaximumMatchingSegmentation();
+        List<SegmentToken> tokens = segmentation.process("研究生命的起源");
         assertNotNull(tokens);
         System.out.println(tokens);
     }
