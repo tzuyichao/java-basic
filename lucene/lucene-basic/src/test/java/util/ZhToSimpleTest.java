c0d625c1b13251bf5edae2a080e563036a43b6cd
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-05-30 15:11:03 +0800

    converter

--- lucene/lucene-basic/src/test/java/util/ZhToSimpleTest.java
+++ lucene/lucene-basic/src/test/java/util/ZhToSimpleTest.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 public class ZhToSimpleTest {
     @Test
     public void test_simple() {
-        String original = "生命不息，奮鬥不止。台達電子";
+        String original = "生命不息，奮鬥不止。";
+        String delta = "台達電子";
         System.out.println(ZhToSimple.convertToSimple(original));
+        System.out.println(ZhToSimple.convertToSimple(delta));
     }
 }
