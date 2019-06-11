a38ded31d6448c4b557d0c61185de79a072d952f
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-06-11 10:09:37 +0800

    add Error Listener for handle syntax error

---
+++ antlr/tiny/src/main/java/listener/TinyErrorListener.java
@@ -0,0 +1,47 @@
+package listener;
+
+import org.antlr.v4.runtime.ANTLRErrorListener;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.Collections;
+import java.util.List;
+
+public class TinyErrorListener implements ANTLRErrorListener {
+    private boolean isSyntaxError = false;
+    private List<String> syntaxErrorMessages = new ArrayList<>();
+
+    public boolean isSyntaxError() {
+        return isSyntaxError;
+    }
+
+    public List<String> getSyntaxErrorMessages() {
+        return Collections.unmodifiableList(syntaxErrorMessages);
+    }
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+        isSyntaxError = true;
+        syntaxErrorMessages.add(msg);
+    }
+
+    @Override
+    public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact, BitSet ambigAlts, ATNConfigSet configs) {
+
+    }
+
+    @Override
+    public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex, BitSet conflictingAlts, ATNConfigSet configs) {
+
+    }
+
+    @Override
+    public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction, ATNConfigSet configs) {
+
+    }
+}
--- antlr/tiny/src/main/java/main/Tiny.java
+++ antlr/tiny/src/main/java/main/Tiny.java
@@ -1,5 +1,6 @@
 package main;
 
+import listener.TinyErrorListener;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -16,15 +17,25 @@ import java.nio.file.Paths;
 public class Tiny {
     public static void main(String[] args) {
         try {
+            TinyErrorListener tinyErrorListener = new TinyErrorListener();
             CharStream input = CharStreams.fromPath(Paths.get("src/test/resources/example1.txt"));
             tiny.TinyLexer lexer = new TinyLexer(input);
+            lexer.removeErrorListeners();
+            lexer.addErrorListener(tinyErrorListener);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             TinyParser parser = new TinyParser(tokens);
+            parser.removeErrorListeners();
+            parser.addErrorListener(tinyErrorListener);
             ParseTree parseTree = parser.program();
 
-            System.out.println("LISP:");
-            System.out.println(parseTree.toStringTree(parser));
-            System.out.println();
+            if(!tinyErrorListener.isSyntaxError()) {
+                System.out.println("LISP:");
+                System.out.println(parseTree.toStringTree(parser));
+                System.out.println();
+            } else {
+                System.out.println("Syntax Error");
+                System.out.println(tinyErrorListener.getSyntaxErrorMessages());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
