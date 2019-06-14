3416dd0292ea01a8c6f67d7cde5c0886e3995fc1
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-06-14 11:01:40 +0800

    add ik-analyzer test

--- lucene/lucene-basic/src/main/java/analyzer/TestSmartCN.java
+++ lucene/lucene-basic/src/main/java/analyzer/TestSmartCN.java
@@ -6,6 +6,7 @@ import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import java.io.IOException;
 import java.util.Iterator;
@@ -14,26 +15,37 @@ import java.util.Iterator;
  * 來源: https://blog.csdn.net/xxpsw/article/details/78902312
  */
 public class TestSmartCN {
-    public static void print(String text, Analyzer analyzer) throws IOException {
+    private static void print(String text, Analyzer analyzer) throws IOException {
         TokenStream tokenStream = analyzer.tokenStream("content", text);
         CharTermAttribute attribute = tokenStream.addAttribute(CharTermAttribute.class);
         tokenStream.reset();
         while(tokenStream.incrementToken()) {
-            System.out.println(new String(attribute.toString()));
+            System.out.println(attribute.toString());
         }
     }
 
     public static void main(String[] args) throws Exception {
-        String text = "Lucene自带多种分词器，其中对中文分词支持比较好的是smartcn。但smartcn不支援自订词库。";
-        System.out.println("Standard Analyzer");
+        String text = "Lucene自带多种分词器，其中对中文分词支持比较好的是smartcn。但smartcn不支援自订词库。上平車這是自訂詞典的詞。";
+        System.out.println("Standard Analyzer:");
         StandardAnalyzer standardAnalyzer = new StandardAnalyzer();
         print(text, standardAnalyzer);
 
-        System.out.println("Smart Chinese Analyzer");
+        System.out.println("Smart Chinese Analyzer:");
         SmartChineseAnalyzer smartChineseAnalyzer = new SmartChineseAnalyzer();
         print(text, smartChineseAnalyzer);
 
-        System.out.println("Smart Chinese Analyzer with Custom Stop words");
+        System.out.println("ik-Analyzer:");
+        Analyzer ikAnalyzer = new IKAnalyzer(true);
+        try (TokenStream tokenStream = ikAnalyzer.tokenStream("testField", text)) {
+            CharTermAttribute term = tokenStream.addAttribute(CharTermAttribute.class);
+            tokenStream.reset();
+            while(tokenStream.incrementToken()) {
+                System.out.println(term.toString());
+            }
+            tokenStream.end();
+        }
+
+        System.out.println("Smart Chinese Analyzer with Custom Stop words:");
         CharArraySet charArraySet = new CharArraySet(0, true);
         Iterator<Object> iterator = SmartChineseAnalyzer.getDefaultStopSet().iterator();
         while(iterator.hasNext()) {
--- lucene/lucene-basic/src/main/java/word2vec/TestText8.java
+++ lucene/lucene-basic/src/main/java/word2vec/TestText8.java
@@ -3,6 +3,9 @@ package word2vec;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.word2vec.Word2Vec;
 
+/**
+ * 這個程式的目的是測試gensim訓練出來的模型，能不能在java透過dl4j讀進來做預測
+ */
 public class TestText8 {
     public static void main(String[] args) {
         Word2Vec word2Vec = WordVectorSerializer.readWord2VecModel("/Users/tzuyichao/lab/python/basic/text8.bin");
