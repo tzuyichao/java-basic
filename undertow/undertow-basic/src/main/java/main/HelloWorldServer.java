6fe5c6e95b0504340c44cd054f53a471e7005641
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-06-27 11:02:03 +0800

    first add

---
+++ undertow/undertow-basic/src/main/java/main/HelloWorldServer.java
@@ -0,0 +1,17 @@
+package main;
+
+import io.undertow.Undertow;
+import io.undertow.util.Headers;
+
+public class HelloWorldServer {
+    public static void main(String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(exchange -> {
+                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                    exchange.getResponseSender().send("Hello World");
+                })
+                .build();
+        server.start();
+    }
+}
