8dc8951c5e9f6ddbbbe6eecb852ae4ba68d3794e
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-06-28 11:07:27 +0800

    first add

---
+++ lucene/solrj-first/src/main/java/main/DeleteAllDocument.java
@@ -0,0 +1,29 @@
+package main;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.ConcurrentUpdateSolrClient;
+//import org.apache.solr.client.solrj.impl.HttpSolrClient;
+
+import java.io.IOException;
+
+public class DeleteAllDocument {
+    public static void main(String[] args) {
+        String solrCoreUrl = "http://localhost:8983/solr/db";
+        // SolrClient solrClient = new HttpSolrClient.Builder(solrCoreUrl).build();
+        SolrClient solrClient = new ConcurrentUpdateSolrClient.Builder(solrCoreUrl).build();
+
+        try {
+            solrClient.deleteByQuery("*");
+
+            solrClient.commit();
+            System.out.println("Delete document complete.");
+        } catch(IOException | SolrServerException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                solrClient.close();
+            } catch(IOException e) {}
+        }
+    }
+}
