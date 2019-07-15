27e9149c317fcfcbc64863898557590fcbde8ad5
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-07-15 11:09:30 +0800

    reload core via solrj

--- lucene/solrj-first/src/main/java/core/CoreReload.java
+++ lucene/solrj-first/src/main/java/core/CoreReload.java
@@ -11,32 +11,27 @@ import org.apache.solr.common.util.SimpleOrderedMap;
 import java.io.IOException;
 import java.util.Map;
 
-public class CoreStatus {
-    private static final String STATUS = "status";
+public class CoreReload {
+    private static final String RequestHeader = "responseHeader";
 
-    private static void printCoreStatus(SolrClient client, String coreName) throws SolrServerException, IOException {
+    private static void reloadCore(SolrClient client, String coreName) throws SolrServerException, IOException {
         CoreAdminRequest request = new CoreAdminRequest();
         // 如果不設定core name則會列出所有cores
         request.setCoreName(coreName);
-        request.setAction(CoreAdminParams.CoreAdminAction.STATUS);
+        request.setAction(CoreAdminParams.CoreAdminAction.RELOAD);
         NamedList<Object> result = client.request(request);
         System.out.println(result);
 
-        SimpleOrderedMap map = (SimpleOrderedMap) result.get(STATUS);
+        SimpleOrderedMap map = (SimpleOrderedMap) result.get(RequestHeader);
         for(Object obj : map) {
             Map.Entry entry = (Map.Entry) obj;
-            SimpleOrderedMap coreMap = (SimpleOrderedMap) entry.getValue();
-            for(Object object : coreMap) {
-                Map.Entry coreMapEntry = (Map.Entry) object;
-                System.out.println(coreMapEntry.getKey() + ": " + coreMapEntry.getValue());
-            }
-            System.out.println();
+            System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 
     public static void main(String[] args) {
         try (SolrClient solrClient = new HttpSolrClient.Builder("http://localhost:8983/solr/").build()) {
-            printCoreStatus(solrClient, "db");
+            reloadCore(solrClient, "db");
         } catch(SolrServerException e) {
             e.printStackTrace();
         } catch (IOException e) {
