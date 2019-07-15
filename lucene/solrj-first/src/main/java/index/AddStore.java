ea6d28ec986ee8ccffc619d362d0d9be69314907
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-07-15 15:20:52 +0800

    add document and hard commit

---
+++ lucene/solrj-first/src/main/java/index/AddStore.java
@@ -0,0 +1,30 @@
+package index;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.request.UpdateRequest;
+
+import java.io.IOException;
+
+public class AddStore {
+    public static void main(String[] args) {
+        try(SolrClient solrClient = new HttpSolrClient.Builder("http://localhost:8983/solr/").build()) {
+            Store store1 = new Store();
+            store1.setId("t2");
+            store1.setName("麵屋武藏-神山");
+            store1.setCity("Taipei");
+            store1.setType("Japan Food");
+            store1.setState("Taiwan");
+            store1.getTags().add("ramen");
+            store1.getTags().add("sit down");
+            store1.setPrice(9.0);
+            solrClient.addBean("db", store1);
+            solrClient.commit("db");
+        } catch(SolrServerException e) {
+            e.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ lucene/solrj-first/src/main/java/index/Store.java
@@ -0,0 +1,85 @@
+package index;
+
+import org.apache.solr.client.solrj.beans.Field;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    @Field
+    private String id;
+
+    @Field
+    private String name;
+
+    @Field
+    private String city;
+
+    @Field
+    private String type;
+
+    @Field
+    private String state;
+
+    @Field
+    private List<String> tags = new ArrayList<>();
+
+    @Field
+    private double price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
