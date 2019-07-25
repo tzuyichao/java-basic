c2b1118a70dfe723a042acbb79c358bdeff1a428
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-07-25 13:29:59 +0800

    lab

---
+++ hazelcast/hello/src/main/java/client/HazelcastClientLab.java
@@ -0,0 +1,24 @@
+package client;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+
+import java.util.Map;
+import java.util.Queue;
+
+public class HazelcastClientLab {
+    public static void main(String[] args) {
+        ClientConfig clientConfig = new ClientConfig();
+        HazelcastInstance hazelcastInstance = HazelcastClient.newHazelcastClient(clientConfig);
+
+        Map<Integer, String> clusterMap = hazelcastInstance.getMap("MyMap");
+        Queue<String> clusterQueue = hazelcastInstance.getQueue("MyQueue");
+
+        System.out.println("Map Value: " + clusterMap.get(1));
+        System.out.println("Queue size: " + clusterQueue.size());
+        System.out.println("Queue value 1: " + clusterQueue.poll());
+        System.out.println("Queue value 2: " + clusterQueue.poll());
+        System.out.println("Queue size: " + clusterQueue.size());
+    }
+}
