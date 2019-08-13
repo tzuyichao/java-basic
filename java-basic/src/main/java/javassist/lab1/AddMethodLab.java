4edc7979994ad6761d38bc4fdd136c597f38c9f8
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-08-13 14:34:55 +0800

    with argument

--- java-basic/src/main/java/javassist/lab1/AddMethodLab.java
+++ java-basic/src/main/java/javassist/lab1/AddMethodLab.java
@@ -9,14 +9,14 @@ public class AddMethodLab {
     public static void main(String[] args) {
         try {
             CtClass ctClass = ClassPool.getDefault().get("javassist.lab1.Point");
-            CtMethod helloMethod = CtNewMethod.make("public void hello() {\nSystem.out.println(\"Hello, World!\");\n}", ctClass);
+            CtMethod helloMethod = CtNewMethod.make("public void hello(String msg) {\nSystem.out.println(\"Hello, World! \" + msg + \"!\");\n}", ctClass);
             ctClass.addMethod(helloMethod);
 
             Class pointClass = ctClass.toClass();
             Point point = (Point)pointClass.getDeclaredConstructor().newInstance();
-            Method helloMeth = pointClass.getDeclaredMethod("hello");
+            Method helloMeth = pointClass.getDeclaredMethod("hello", String.class);
 
-            helloMeth.invoke(point);
+            helloMeth.invoke(point, "testing");
 
             System.out.println("done.");
         } catch(NotFoundException e) {
