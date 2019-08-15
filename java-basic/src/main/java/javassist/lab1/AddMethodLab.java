ef22be141830a16442fcf070b56c6a278186a96a
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-08-15 16:01:10 +0800

    add method labs

--- java-basic/src/main/java/javassist/lab1/AddMethodLab.java
+++ java-basic/src/main/java/javassist/lab1/AddMethodLab.java
@@ -12,10 +12,28 @@ public class AddMethodLab {
             CtMethod helloMethod = CtNewMethod.make("public void hello(String msg) {\nSystem.out.println(\"Hello, World! \" + msg + \"!\");\n}", ctClass);
             ctClass.addMethod(helloMethod);
 
+            /**
+             * public CtMethod(CtClass returnType, String mname,
+             *                     CtClass[] parameters, CtClass declaring) {
+             *         this(null, declaring);
+             *         ConstPool cp = declaring.getClassFile2().getConstPool();
+             *         String desc = Descriptor.ofMethod(returnType, parameters);
+             *         methodInfo = new MethodInfo(cp, mname, desc);
+             *         setModifiers(Modifier.PUBLIC | Modifier.ABSTRACT);
+             *     }
+             */
             CtMethod moveXMethod = new CtMethod(CtClass.intType, "moveX", new CtClass[] {CtClass.intType}, ctClass);
+            // 拿掉method的abstract
+            moveXMethod.setModifiers(moveXMethod.getModifiers() & ~Modifier.ABSTRACT);
+            System.out.println("moveX() Modifier: " + moveXMethod.getModifiers());
+            System.out.println("Before add method to Point Class Modifier: " + ctClass.getModifiers());
             ctClass.addMethod(moveXMethod);
-            moveXMethod.setBody("{ x += $1; System.out.println(\"current x=\" + $0.x); return x; }");
-            ctClass.setModifiers(ctClass.getModifiers() & ~Modifier.ABSTRACT);
+            System.out.println("Before method setBody to Point Class Modifier: " + ctClass.getModifiers());
+            moveXMethod.setBody("{ x += $1; System.out.println(\"current x=\" + $0.x + \" \" + javassist.CtClass#intType.getName()); return x; }");
+            System.out.println("After method setBody and add method to Point Class Modifier: " + ctClass.getModifiers());
+            System.out.println("moveX() Modifier: " + moveXMethod.getModifiers());
+            // 預設CtMethod constructor的modifier是public abstract，在addMethod()後，class的Modifier會變成abstract所以需要重新設定。
+            //ctClass.setModifiers(ctClass.getModifiers() & ~Modifier.ABSTRACT);
 
             Class pointClass = ctClass.toClass();
             //Point point = (Point)pointClass.getDeclaredConstructor().newInstance();
