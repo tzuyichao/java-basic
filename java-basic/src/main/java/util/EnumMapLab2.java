b560602f116a20302b499b5d8f9e3f03ff4b75f1
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-09-09 17:15:52 +0800

    Item 37 part II

---
+++ java-basic/src/main/java/util/EnumMapLab2.java
@@ -0,0 +1,37 @@
+package util;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
+
+public class EnumMapLab2 {
+    public static void main(String[] args) {
+        System.out.println(Phase.LIQUID + " : " +  Phase.Transition.from(Phase.LIQUID, Phase.GAS));
+    }
+}
+
+enum Phase {
+    SOLID, LIQUID, GAS;
+
+    public enum Transition {
+        MELT(SOLID, LIQUID), FREEZE(LIQUID, SOLID), BOIL(LIQUID, GAS);
+
+        private final Phase from;
+        private final Phase to;
+
+        Transition(Phase from, Phase to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        private static final Map<Phase, Map<Phase, Transition>> m = Stream.of(values())
+                .collect(groupingBy(t -> t.from, () -> new EnumMap<>(Phase.class), toMap(t -> t.to, t -> t, (x, y) -> y, () -> new EnumMap<>(Phase.class))));
+
+        public static Transition from(Phase from, Phase to) {
+            return m.get(from).get(to);
+        }
+    }
+}
