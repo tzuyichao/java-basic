b440d95dd1e0cf490cc7e2d0ab0850e3cbfa8b1f
TERENCE.CHAO <terence.chao@deltaww.com>
2019-11-15 16:57:47 +0800

    some test

--- java-basic/src/main/java/basic/ArraysLab.java
+++ java-basic/src/main/java/basic/ArraysLab.java
@@ -1,9 +1,32 @@
 package basic;
 
 import java.util.Arrays;
-import java.util.Spliterators;
+import java.util.Comparator;
 
 public class ArraysLab {
+    private static void test1() {
+        Integer[] arr = new Integer[] {2, 0, 10};
+        Arrays.sort(arr, Comparator.reverseOrder());
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static int test2(int[] in) {
+        Integer[] arr = Arrays.stream(in).boxed().toArray(Integer[]::new);
+        Arrays.sort(arr, Comparator.reverseOrder());
+        System.out.println(Arrays.toString(arr));
+        int result = 0;
+        if(arr.length == 1) {
+            return result;
+        } if(arr.length == 2) {
+            return arr[0] -arr[1];
+        } else {
+            for (int i = 0; i < arr.length-1; i++) {
+                result += (arr[i] - arr[i+1]);
+            }
+            return result;
+        }
+    }
+
     public static void main(String[] args) {
         String[] arr = new String[] {"1", "2", "3", "4"};
         Arrays.spliterator(arr, 0, arr.length-1).forEachRemaining(item -> {
@@ -15,5 +38,9 @@ public class ArraysLab {
 
         String test2 = "1    3";
         System.out.println(test2.replaceAll("[ ]+", " "));
+
+        test1();
+
+        System.out.println(test2(new int[]{1, 2, 10}));
     }
 }
