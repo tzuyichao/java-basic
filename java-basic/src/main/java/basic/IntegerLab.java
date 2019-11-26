2df5f88dc78b1ee8622dfc55addd958acda2c335
TERENCE.CHAO <terence.chao@deltaww.com>
2019-11-26 13:52:37 +0800

    boxing and unboxing語法糖

---
+++ java-basic/src/main/java/basic/IntegerLab.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class IntegerLab {
+    public static void main(String[] args) {
+        Integer i = 1;    // INVOKESTATIC java/lang/Integer.valueOf (I)Ljava/lang/Integer;
+        int i2 = i;       // INVOKEVIRTUAL java/lang/Integer.intValue ()I
+
+    }
+}
