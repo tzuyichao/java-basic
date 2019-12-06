b12dd855cc70def36e8b9e25ccd7928510792db7
TERENCE.CHAO <terence.chao@deltaww.com>
2019-12-06 13:17:36 +0800

    allocators and default allocator

--- netty/discard/src/test/java/bytebuf/ReadWriteTest.java
+++ netty/discard/src/test/java/bytebuf/ReadWriteTest.java
@@ -2,23 +2,22 @@ package bytebuf;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.buffer.UnpooledByteBufAllocator;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class ReadWriteTest {
     void inspect(ByteBuf buffer) {
-        log.info(buffer.toString());
+        log.info("{}", buffer);
         log.info("is readable: {}", buffer.isReadable());
         log.info("is writable: {}", buffer.isWritable());
-//        log.info("ByteBuf readerIndex: {}", buffer.readerIndex());
-//        log.info("ByteBuf writerIndex: {}", buffer.writerIndex());
-//        log.info("ByteBuf capacity: {}", buffer.capacity());
-//        log.info("ByteBuf max capacity: {}", buffer.maxCapacity());
+        log.info("reference count: {}", buffer.refCnt());
     }
 
     @Test
-    void basic_operation() {
+    void test_basic_operation() {
         log.info("===== initial ByteBuf =====");
         ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer(9, 100);
         inspect(buffer);
@@ -27,6 +26,34 @@ public class ReadWriteTest {
         inspect(buffer);
         log.info("===== read byte =====");
         buffer.readByte();
+        inspect(buffer);
+    }
+
+    @Test
+    void test_reference_count() {
+        log.info("===== initial ByteBuf =====");
+        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer(9, 100);
+        inspect(buffer);
+        log.info("===== retain =====");
+        buffer.retain();
+        inspect(buffer);
+        log.info("===== retain =====");
+        buffer.retain();
+        inspect(buffer);
+        log.info("===== release =====");
+        buffer.release();
+        inspect(buffer);
+    }
+
+    @Test
+    void test_allocator() {
+        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer(9, 100);
+        inspect(buffer);
+        buffer = ByteBufAllocator.DEFAULT.buffer();
+        inspect(buffer);
+        buffer = UnpooledByteBufAllocator.DEFAULT.heapBuffer();
+        inspect(buffer);
+        buffer = PooledByteBufAllocator.DEFAULT.directBuffer();
         inspect(buffer);
     }
 }
