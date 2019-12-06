cc883189400474a79da6dcaa03d543dc5128ec20
TERENCE.CHAO <terence.chao@deltaww.com>
2019-12-06 10:40:50 +0800

    basic ByteBuf operation lab

--- netty/discard/src/test/java/bytebuf/ReadWriteTest.java
+++ netty/discard/src/test/java/bytebuf/ReadWriteTest.java
@@ -1,4 +1,32 @@
 package bytebuf;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
 public class ReadWriteTest {
+    void inspect(ByteBuf buffer) {
+        log.info(buffer.toString());
+        log.info("is readable: {}", buffer.isReadable());
+        log.info("is writable: {}", buffer.isWritable());
+//        log.info("ByteBuf readerIndex: {}", buffer.readerIndex());
+//        log.info("ByteBuf writerIndex: {}", buffer.writerIndex());
+//        log.info("ByteBuf capacity: {}", buffer.capacity());
+//        log.info("ByteBuf max capacity: {}", buffer.maxCapacity());
+    }
+
+    @Test
+    void basic_operation() {
+        log.info("===== initial ByteBuf =====");
+        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer(9, 100);
+        inspect(buffer);
+        log.info("===== write 4 bytes =====");
+        buffer.writeBytes(new byte[] {1, 2, 3, 4});
+        inspect(buffer);
+        log.info("===== read byte =====");
+        buffer.readByte();
+        inspect(buffer);
+    }
 }
