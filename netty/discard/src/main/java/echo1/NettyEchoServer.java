7c8288888f32640ae5e327a2a0b0becef0e85fb0
TERENCE.CHAO <terence.chao@deltaww.com>
2019-12-09 17:12:14 +0800

    partial echo1 server

---
+++ netty/discard/src/main/java/echo1/NettyEchoServer.java
@@ -0,0 +1,38 @@
+package echo1;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public final class NettyEchoServer {
+    public void runServer() {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup, workerGroup);
+        b.channel(NioServerSocketChannel.class);
+        b.option(ChannelOption.TCP_NODELAY, true);
+        b.handler(new LoggingHandler(LogLevel.INFO));
+        b.childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel ch) throws Exception {
+                ChannelPipeline pipeline = ch.pipeline();
+                pipeline.addLast(NettyEchoServerHandler.INSTANCE);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new NettyEchoServer().runServer();
+    }
+}
---
+++ netty/discard/src/main/java/echo1/NettyEchoServerHandler.java
@@ -0,0 +1,17 @@
+package echo1;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ChannelHandler.Sharable
+public final class NettyEchoServerHandler extends ChannelInboundHandlerAdapter {
+    public static final NettyEchoServerHandler INSTANCE = new NettyEchoServerHandler();
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+    }
+}
