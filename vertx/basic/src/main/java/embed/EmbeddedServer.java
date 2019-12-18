6f3e3e909027f17325b6501f324dd2bb3f65d206
TERENCE.CHAO <terence.chao@deltaww.com>
2019-12-18 13:55:46 +0800

    add logger

--- vertx/basic/src/main/java/embed/EmbeddedServer.java
+++ vertx/basic/src/main/java/embed/EmbeddedServer.java
@@ -1,9 +1,14 @@
 package embed;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 public final class EmbeddedServer {
+    private static final Logger log = LoggerFactory.getLogger(EmbeddedServer.class);
+
     public static void main(String[] args) {
         Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("hello, world!")).listen(8080);
+        log.info("Server Started.");
     }
 }
