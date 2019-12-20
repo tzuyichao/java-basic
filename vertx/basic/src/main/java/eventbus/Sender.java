bbdd68d09a25e2766e9267e04c5e40949a940f8c
TzuYi Chao <tzuyichao@mail2000.com.tw>
2019-12-20 22:05:13 +0800

    partial eventbus

---
+++ vertx/basic/src/main/java/eventbus/Sender.java
@@ -0,0 +1,11 @@
+package eventbus;
+
+import io.vertx.core.AbstractVerticle;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Sender extends AbstractVerticle {
+    public static void main(String[] args) {
+        log.info("done.");
+    }
+}
