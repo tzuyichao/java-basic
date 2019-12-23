9d1bed815ec82ba8957fba65268711d2e8daebec
TERENCE.CHAO <terence.chao@deltaww.com>
2019-12-23 10:52:39 +0800

    protobuf marshal test

--- netty/discard/src/test/java/protocol/ProtobufTest.java
+++ netty/discard/src/test/java/protocol/ProtobufTest.java
@@ -3,6 +3,11 @@ package protocol;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Slf4j
@@ -16,5 +21,54 @@ public class ProtobufTest {
 
         assertNotNull(message);
         log.info("message: {}", message);
+    }
+
+    private MsgProtos.Msg buildMsg(int id, String message) {
+        MsgProtos.Msg.Builder builder = MsgProtos.Msg.newBuilder();
+        builder.setId(id);
+        builder.setContent(message);
+        MsgProtos.Msg msg = builder.build();
+        return msg;
+    }
+
+    @Test
+    void test_marshal_unmarshal1() throws IOException {
+        MsgProtos.Msg msg = buildMsg(1, "test");
+        byte[]  byteArrayContent = msg.toByteArray();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        outputStream.write(byteArrayContent);
+        byte[] data = outputStream.toByteArray();
+
+        MsgProtos.Msg readMsg = MsgProtos.Msg.parseFrom(data);
+        assertEquals(msg.getId(), readMsg.getId());
+        assertEquals(msg.getContent(), readMsg.getContent());
+    }
+
+    @Test
+    void test_marshal_unmarshal2() throws IOException {
+        MsgProtos.Msg msg = buildMsg(2, "測試");
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        msg.writeTo(outputStream);
+        byte[] data = outputStream.toByteArray();
+
+        MsgProtos.Msg readMsg = MsgProtos.Msg.parseFrom(data);
+        assertEquals(msg.getId(), readMsg.getId());
+        assertEquals(msg.getContent(), readMsg.getContent());
+        log.info("msg content: {}", msg.getContent());
+    }
+
+    @Test
+    void test_marshal_unmarshal3() throws IOException {
+        MsgProtos.Msg msg = buildMsg(3, "測試 gogogo");
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        msg.writeDelimitedTo(outputStream);
+        byte[] data = outputStream.toByteArray();
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(data);
+        MsgProtos.Msg readMsg = MsgProtos.Msg.parseDelimitedFrom(inputStream);
+        assertEquals(msg.getId(), readMsg.getId());
+        assertEquals(msg.getContent(), readMsg.getContent());
+        log.info("msg content: {}", msg.getContent());
     }
 }
