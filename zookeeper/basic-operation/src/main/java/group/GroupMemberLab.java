dca1d4a53307adfbdb6e4de1a3e97e784a9a50f6
TzuYi Chao <tzuyichao@mail2000.com.tw>
2020-01-18 22:50:53 +0800

    test GroupMember

--- zookeeper/basic-operation/src/main/java/group/GroupMemberLab.java
+++ zookeeper/basic-operation/src/main/java/group/GroupMemberLab.java
@@ -2,12 +2,14 @@ package group;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.nodes.GroupMember;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
@@ -16,18 +18,32 @@ import java.util.concurrent.TimeUnit;
  */
 @Slf4j
 public class GroupMemberLab {
+    public static final String ZK_ADDRESS = "127.0.0.1:2181";
+
     public static void main(String[] args) {
         final int workerSize = 10;
-        Executor executor = Executors.newFixedThreadPool(workerSize);
-
+        ExecutorService executor = Executors.newFixedThreadPool(workerSize);
+        ExponentialBackoffRetry retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        try (CuratorFramework client = CuratorFrameworkFactory.builder()
+                .connectString(ZK_ADDRESS)
+                .retryPolicy(retryPolicy)
+                .build()) {
+            client.start();
+            for(int i=0; i<10; i++) {
+                executor.submit(new TestGroupMember(client, i));
+            }
+            TimeUnit.MINUTES.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     static class TestGroupMember implements Runnable {
         public static final String MEMBERSHIP_PATH = "/test/test-group";
         private GroupMember groupMember;
 
-        public TestGroupMember(CuratorFramework curatorFramework) {
-            groupMember = new GroupMember(curatorFramework, MEMBERSHIP_PATH, Thread.currentThread().getName(), "test".getBytes(StandardCharsets.UTF_8));
+        public TestGroupMember(CuratorFramework curatorFramework, int id) {
+            groupMember = new GroupMember(curatorFramework, MEMBERSHIP_PATH, "id:" + id, "test".getBytes(StandardCharsets.UTF_8));
             groupMember.start();
         }
 
