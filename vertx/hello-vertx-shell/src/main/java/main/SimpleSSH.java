03833761a759bcb76647f07d399c005d9f677e40
TERENCE.CHAO <terence.chao@deltaww.com>
2020-01-21 10:18:35 +0800

    hello ssh

---
+++ vertx/hello-vertx-shell/src/main/java/main/SimpleSSH.java
@@ -0,0 +1,29 @@
+package main;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.net.JksOptions;
+import io.vertx.ext.auth.shiro.ShiroAuthOptions;
+import io.vertx.ext.auth.shiro.ShiroAuthRealmType;
+import io.vertx.ext.shell.ShellService;
+import io.vertx.ext.shell.ShellServiceOptions;
+import io.vertx.ext.shell.term.SSHTermOptions;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * https://vertx.io/docs/vertx-shell/java/#_authentication
+ */
+@Slf4j
+public class SimpleSSH {
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        ShellService shellService = ShellService.create(vertx, new ShellServiceOptions()
+            .setSSHOptions(new SSHTermOptions().setHost("localhost").setPort(5000)
+                    .setKeyPairOptions(new JksOptions().setPath("ssh.jks").setPassword("secret"))
+                    .setAuthOptions(new ShiroAuthOptions().setType(ShiroAuthRealmType.PROPERTIES).setConfig(new JsonObject().put("properties_path", "file:auth.properties")))
+            )
+        );
+        log.info("Starting ssh server...");
+        shellService.start();
+    }
+}
