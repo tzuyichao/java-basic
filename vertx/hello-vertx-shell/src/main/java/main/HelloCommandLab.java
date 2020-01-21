8e7de78a52367fbe42950f9a8872e7a67f0f45c0
TERENCE.CHAO <terence.chao@deltaww.com>
2020-01-21 13:47:19 +0800

    hello command

---
+++ vertx/hello-vertx-shell/src/main/java/main/HelloCommandLab.java
@@ -0,0 +1,38 @@
+package main;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.net.JksOptions;
+import io.vertx.ext.auth.shiro.ShiroAuthOptions;
+import io.vertx.ext.auth.shiro.ShiroAuthRealmType;
+import io.vertx.ext.shell.ShellService;
+import io.vertx.ext.shell.ShellServiceOptions;
+import io.vertx.ext.shell.command.CommandBuilder;
+import io.vertx.ext.shell.command.CommandRegistry;
+import io.vertx.ext.shell.term.SSHTermOptions;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HelloCommandLab {
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        CommandBuilder builder = CommandBuilder.command("hello");
+        builder.processHandler(process -> {
+            process.write("Hello, World!\n");
+            process.end();
+        });
+        CommandRegistry registry = CommandRegistry.getShared(vertx);
+        registry.registerCommand(builder.build(vertx));
+
+        ShellService shellService = ShellService.create(vertx, new ShellServiceOptions()
+                .setSSHOptions(new SSHTermOptions().setHost("localhost").setPort(5000)
+                        .setKeyPairOptions(new JksOptions().setPath("ssh.jks").setPassword("secret"))
+                        .setAuthOptions(new ShiroAuthOptions()
+                                .setType(ShiroAuthRealmType.PROPERTIES)
+                                .setConfig(new JsonObject().put("properties_path", "file:auth.properties")))
+                )
+        );
+        log.info("Starting ssh server...");
+        shellService.start();
+    }
+}
