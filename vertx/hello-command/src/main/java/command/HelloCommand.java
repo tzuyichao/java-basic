499fb1d672f133b5b5020fc80d8cb9eaa1381c92
TERENCE.CHAO <terence.chao@deltaww.com>
2020-01-22 12:46:10 +0800

    part 1

--- vertx/hello-command/src/main/java/command/HelloCommand.java
+++ vertx/hello-command/src/main/java/command/HelloCommand.java
@@ -2,8 +2,17 @@ package command;
 
 import io.vertx.ext.shell.command.AnnotatedCommand;
 import io.vertx.ext.shell.command.CommandProcess;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 public class HelloCommand extends AnnotatedCommand {
+    private String name;
+
+    public HelloCommand() {
+        this.name = "hello";
+    }
+
     @Override
     public void process(CommandProcess process) {
         process.write("Hello, World!\n");
