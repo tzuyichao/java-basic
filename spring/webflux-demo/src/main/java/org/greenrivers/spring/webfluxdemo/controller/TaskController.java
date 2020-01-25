01eff8b47b5d1898fe652af224c6af2c4b863b7f
TzuYi Chao <tzuyichao@mail2000.com.tw>
2020-01-25 23:41:46 +0800

    lab about biz thread pool executor

---
+++ spring/webflux-demo/src/main/java/org/greenrivers/spring/webfluxdemo/controller/TaskController.java
@@ -0,0 +1,36 @@
+package org.greenrivers.spring.webfluxdemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.greenrivers.spring.webfluxdemo.model.Task;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@RestController
+public class TaskController {
+    private static final ThreadPoolExecutor bizThreadPoolExecutor = new ThreadPoolExecutor(
+            8,
+            8,
+            1,
+            TimeUnit.MINUTES,
+            new LinkedBlockingQueue<>(10));
+
+    @GetMapping("tasks")
+    Flux<Task> getTasks() {
+        log.info("----- getTasks -----");
+        return Flux.just("Task 1", "Task 2")
+                .publishOn(Schedulers.fromExecutor(bizThreadPoolExecutor))
+                .map(str -> {
+                    log.info("map {} to Task object", str);
+                    Task task = new Task();
+                    task.setName(str);
+                    return task;
+                });
+    }
+}
