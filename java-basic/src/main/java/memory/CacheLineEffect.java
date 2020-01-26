3119972ccbd5b99273c94a6a299178a23958a9a8
TzuYi Chao <tzuyichao@mail2000.com.tw>
2020-01-26 15:35:49 +0800

    CacheLine Effect

--- java-basic/src/main/java/memory/CacheLineEffect.java
+++ java-basic/src/main/java/memory/CacheLineEffect.java
@@ -7,25 +7,26 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class CacheLineEffect {
+    private static final int Limit = 8;
     static long[][] arr;
     public static void main(String[] args) {
         arr = new long[1024*1024][];
         for(int i=0; i<1024*1024; i++) {
-            arr[i] = new long[8];
-            for(int j=0; j<8; j++) {
+            arr[i] = new long[Limit];
+            for(int j=0; j<Limit; j++) {
                 arr[i][j] = 0L;
             }
         }
         long sum = 0L;
         long mark = System.currentTimeMillis();
         for(int i=0; i<1024*1024; i++) {
-            for(int j=0; j<8; j++) {
+            for(int j=0; j<Limit; j++) {
                 sum += arr[i][j];
             }
         }
         log.info("Loop time: {} ms", (System.currentTimeMillis() - mark));
         mark = System.currentTimeMillis();
-        for(int i=0; i<8; i++) {
+        for(int i=0; i<Limit; i++) {
             for(int j=0; j<1024*1024; j++) {
                 sum += arr[j][i];
             }
