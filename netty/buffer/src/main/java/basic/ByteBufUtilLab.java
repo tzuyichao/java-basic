57af080e3b758f127481ef88f39ad6f43cbd94c0
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-04 10:50:57 +0800

    ByteBuf Lab: ByteBufUtil

---
+++ netty/buffer/src/main/java/basic/ByteBufUtilLab.java
@@ -0,0 +1,21 @@
+package basic;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class ByteBufUtilLab {
+    public static void main(String[] args) {
+        ByteBuf buf1 = Unpooled.copiedBuffer("Netty in Action rocks!", StandardCharsets.UTF_8);
+        ByteBuf buf2 = Unpooled.copiedBuffer("Netty in Action rocks!", StandardCharsets.UTF_8);
+        log.info("buf1: {}", buf1);
+        log.info("buf2: {}", buf2);
+        log.info("equals? {}", ByteBufUtil.equals(buf1, buf2));
+
+        log.info("hexDump: {}", ByteBufUtil.hexDump("abc".getBytes()));
+    }
+}
