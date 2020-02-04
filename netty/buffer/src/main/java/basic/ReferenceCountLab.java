492ab3d14442493a51838d8c33a24f4a71a861aa
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-04 11:04:08 +0800

    ByteBuf Lab: Reference Count

---
+++ netty/buffer/src/main/java/basic/ReferenceCountLab.java
@@ -0,0 +1,27 @@
+package basic;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class ReferenceCountLab {
+    public static void main(String[] args) {
+        ByteBuf buf = Unpooled.copiedBuffer("Netty in Action rocks!", StandardCharsets.UTF_8);
+        log.info("buf rc: {}", buf.refCnt());
+        ByteBuf dup_duf = buf.duplicate();
+        log.info("buf rc: {}", buf.refCnt());
+        log.info("dup_duf rc: {}", dup_duf.refCnt());
+        ByteBuf copy_duf = buf.copy();
+        log.info("buf rc: {}", buf.refCnt());
+        log.info("copy_duf rc: {}", copy_duf.refCnt());
+        ByteBuf retain_duf = buf.retain();
+        log.info("buf rc: {}", buf.refCnt());
+        log.info("retain_duf rc: {}", retain_duf.refCnt());
+        retain_duf.release();
+        log.info("buf rc: {}", buf.refCnt());
+        log.info("retain_duf rc: {}", retain_duf.refCnt());
+    }
+}
