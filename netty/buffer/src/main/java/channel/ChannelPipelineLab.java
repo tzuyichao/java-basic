ea238eb6cac4f5224ebdd45661987cdeb9542419
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-07 13:51:02 +0800

    partial pipeline test

--- netty/buffer/src/main/java/channel/ChannelPipelineLab.java
+++ netty/buffer/src/main/java/channel/ChannelPipelineLab.java
@@ -6,12 +6,17 @@ import io.netty.channel.embedded.EmbeddedChannel;
 
 public class ChannelPipelineLab {
     public static void main(String[] args) {
+        final String handler1_name = "handler1";
         EmbeddedChannel channel = new EmbeddedChannel();
         ChannelPipeline pipeline = channel.pipeline();
-        pipeline.addLast("handler1", new FirstHandler());
+        pipeline.addLast(handler1_name, new FirstHandler());
         pipeline.addFirst("handler2", new SecondHandler());
 
         channel.writeInbound("test");
+
+        pipeline.remove(handler1_name);
+        channel.writeInbound("test2");
+
         channel.close();
     }
 }
