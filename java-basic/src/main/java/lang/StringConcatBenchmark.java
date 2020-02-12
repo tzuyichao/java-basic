26a44ce070cfcc2a58455ffa47d0414c71e40ced
TERENCE.CHAO <terence.chao@deltaww.com>
2020-02-12 15:27:33 +0800

    benchmark on windows

--- java-basic/src/main/java/lang/StringConcatBenchmark.java
+++ java-basic/src/main/java/lang/StringConcatBenchmark.java
@@ -11,10 +11,17 @@ import java.util.concurrent.TimeUnit;
 /**
  * Sample from Java系統性能優化實戰
  *
+ * Mac OS X
  * Benchmark                                           Mode  Cnt    Score    Error  Units
  * StringConcatBenchmark.concat                        avgt   25   24.168 ±  0.397  ns/op
  * StringConcatBenchmark.concatByOptimizeBuilder       avgt   25   21.559 ±  0.080  ns/op
  * StringConcatBenchmark.concatWithoutOptimizeBuilder  avgt   25  169.779 ± 60.746  ns/op
+ *
+ * Windows
+ * Benchmark                                           Mode  Cnt   Score   Error  Units
+ * StringConcatBenchmark.concat                        avgt   25  28.138 ± 1.169  ns/op
+ * StringConcatBenchmark.concatByOptimizeBuilder       avgt   25  26.264 ± 1.268  ns/op
+ * StringConcatBenchmark.concatWithoutOptimizeBuilder  avgt   25  73.675 ± 3.200  ns/op
  */
 @State(Scope.Thread)
 @BenchmarkMode(Mode.AverageTime)
