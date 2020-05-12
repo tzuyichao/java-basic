b5fdac7f3d177d84adf278337981115785f9ce89
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-12 14:03:18 +0800

    hello @Startup

--- quarkus/lifecycle-quickstart/src/main/java/org/acme/lifecycle/NameGenerator.java
+++ quarkus/lifecycle-quickstart/src/main/java/org/acme/lifecycle/NameGenerator.java
@@ -1,10 +1,13 @@
 package org.acme.lifecycle;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @ApplicationScoped
 public class NameGenerator {
+    private AtomicInteger id = new AtomicInteger();
+
     public String createName() {
-        return Thread.currentThread().getName();
+        return "bean-" + id.incrementAndGet();
     }
 }
