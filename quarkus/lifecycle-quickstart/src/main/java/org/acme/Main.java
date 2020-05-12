70d677f135ddefb2c3648cd53f17d349b199502f
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-12 11:32:17 +0800

    QuarkusApplication for application logic

--- quarkus/lifecycle-quickstart/src/main/java/org/acme/Main.java
+++ quarkus/lifecycle-quickstart/src/main/java/org/acme/Main.java
@@ -1,12 +1,22 @@
 package org.acme;
 
 import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain
 public class Main {
     public static void main(String[] args) {
         System.out.println("Running main method");
-        Quarkus.run(args);
+        Quarkus.run(MyApp.class, args);
+    }
+    
+    public static class MyApp implements QuarkusApplication {
+        @Override
+        public int run(String... args) throws Exception {
+            System.out.println("Do startup logic here");
+            Quarkus.waitForExit();
+            return 0;
+        }
     }
 }
