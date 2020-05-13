10a548863709e49eb69a1dcab5c1d6493be47b30
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-13 14:49:35 +0800

    partial

--- netty/tom-rpc/src/main/java/registry/RegistryHandler.java
+++ netty/tom-rpc/src/main/java/registry/RegistryHandler.java
@@ -1,6 +1,74 @@
 package registry;
 
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.java.Log;
+import protocol.InvokerProtocol;
 
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Log
+@ChannelHandler.Sharable
 public class RegistryHandler extends ChannelInboundHandlerAdapter {
+    public static ConcurrentHashMap<String, Object> registryMap = new ConcurrentHashMap<>();
+    private List<String> classNames = new ArrayList<String>();
+
+    public RegistryHandler() {
+        scannerClass("provider");
+        doRegistry();
+    }
+
+    private void scannerClass(String packageName) {
+        URL url = this.getClass().getClassLoader().getResource(packageName.replaceAll("\\.", "/"));
+        File dir = new File(url.getFile());
+        for(File file : dir.listFiles()) {
+            if(file.isDirectory()) {
+                scannerClass(packageName + "." + file.getName());
+            } else {
+                classNames.add(packageName + "." + file.getName().replace(".class", "").trim());
+            }
+        }
+    }
+
+    private void doRegistry() {
+        if(classNames.size() == 0) {
+            return;
+        }
+        for(String className : classNames) {
+            try {
+                Class<?> clazz = Class.forName(className);
+                Class<?> i = clazz.getInterfaces()[0];
+                log.info("Add " + className + " for " + i.getName());
+                registryMap.put(i.getName(), clazz.getDeclaredConstructor().newInstance());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        InvokerProtocol request = (InvokerProtocol) msg;
+        Object result = new Object();
+        if(registryMap.containsKey(request.getClassName())) {
+            Object clazz = registryMap.get(request.getClassName());
+            Method method = clazz.getClass().getMethod(request.getMethodName(), request.getParams());
+            result = method.invoke(clazz, request.getValues());
+        }
+        ctx.write(result);
+        ctx.flush();
+        ctx.close();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
