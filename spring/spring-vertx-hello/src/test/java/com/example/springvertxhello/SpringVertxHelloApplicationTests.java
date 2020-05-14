75a96b3c6e85e8b85e53cbc454c886123867b04c
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-14 08:45:06 +0800

    first add

---
+++ spring/spring-vertx-hello/src/test/java/com/example/springvertxhello/SpringVertxHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springvertxhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVertxHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
