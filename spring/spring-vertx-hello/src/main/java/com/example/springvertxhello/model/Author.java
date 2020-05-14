ab5f0bc90ee271db34d9946fa3535acb2d7df433
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-14 15:00:51 +0800

    upgrade vertx version add @Id for Author

--- spring/spring-vertx-hello/src/main/java/com/example/springvertxhello/model/Author.java
+++ spring/spring-vertx-hello/src/main/java/com/example/springvertxhello/model/Author.java
@@ -3,10 +3,13 @@ package com.example.springvertxhello.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 @Data
 public class Author {
+    @Id
+    private Integer id;
     private String firstName;
     private String lastName;
 }
