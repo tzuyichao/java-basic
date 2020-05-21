37272d8f10bec9997f13a5e218a29566750a1a47
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-21 14:07:48 +0800

    chapter 2 example 1

--- rxjava/rxjava3/basic/src/main/java/core/observable/Ch2_1.java
+++ rxjava/rxjava3/basic/src/main/java/core/observable/Ch2_1.java
@@ -3,6 +3,7 @@ package core.observable;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.observers.DisposableObserver;
+import utils.ConcurrentUtil;
 
 public class Ch2_1 {
     public static void main(String[] args) {
@@ -13,8 +14,20 @@ public class Ch2_1 {
             emitter.onComplete();
         });
 
+        Observable<String> source2 = Observable.create(emitter -> {
+            new Thread(() -> {
+                emitter.onNext("Alpha");
+                ConcurrentUtil.sleep(200);
+                emitter.onNext("Beta");
+                ConcurrentUtil.sleep(200);
+                emitter.onNext("Gamma");
+                ConcurrentUtil.sleep(200);
+                // emitter.onComplete();
+            }).start();
+        });
+
         source.subscribe(s -> {
-            System.out.println(s);
+            System.out.println("RECEIVED: " + s);
         });
 
         source.subscribe(new DisposableObserver<String>() {
@@ -31,6 +44,24 @@ public class Ch2_1 {
             @Override
             public void onComplete() {
                 System.out.println("Done.");
+            }
+        });
+
+        source2.subscribe(new DisposableObserver<String>() {
+            @Override
+            public void onNext(@NonNull String s) {
+                System.out.println(System.currentTimeMillis() + " RECEIVED: " + s);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                System.err.println(e.getMessage());
+            }
+
+            // 如果emitter的onComplete()沒有呼叫，這個也不會被叫到
+            @Override
+            public void onComplete() {
+                System.out.println(System.currentTimeMillis() +" Done.");
             }
         });
     }
