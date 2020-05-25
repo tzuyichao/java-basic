9ef66e25acf60aadc941953b454c96d9c1d0eca6
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-25 10:52:03 +0800

    Single

---
+++ rxjava/rxjava3/basic/src/main/java/core/observable/Ch2_28.java
@@ -0,0 +1,16 @@
+package core.observable;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Single;
+
+public class Ch2_28 {
+    public static void main(String[] args) {
+        Single.just("Hello!")
+                .map(String::length)
+                .subscribe(l -> System.out.println("RECEIVE: " + l),
+                        Throwable::printStackTrace);
+        Observable<String> source = Observable.just("Alpha", "Beta", "Gamma");
+        Single<String> first = source.first("Nil");
+        first.subscribe(s -> System.out.println("RECEIVE: " + s));
+    }
+}
