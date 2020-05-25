1ab2a5d48ef7067136ee2afe1d0344c29c0d9448
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-25 17:11:23 +0800

    distinctUntilChange()

---
+++ rxjava/rxjava3/basic/src/main/java/core/operator/Ch3_11.java
@@ -0,0 +1,11 @@
+package core.operator;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class Ch3_11 {
+    public static void main(String[] args) {
+        Observable.just(1, 1, 1, 2, 2, 3, 3, 2, 1, 1)
+                .distinctUntilChanged()
+                .subscribe(i -> System.out.println("RECEIVE: " + i));
+    }
+}
