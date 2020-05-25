7746ac4b0764870a8547a248dc460bd79d5b1727
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-25 17:08:56 +0800

    distinct()

---
+++ rxjava/rxjava3/basic/src/main/java/core/operator/Ch3_10.java
@@ -0,0 +1,11 @@
+package core.operator;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class Ch3_10 {
+    public static void main(String[] args) {
+        Observable.just("Alpha", "Beta", "Gamma")
+                .distinct(String::length)
+                .subscribe(s -> System.out.println("RECEIVE: " + s));
+    }
+}
