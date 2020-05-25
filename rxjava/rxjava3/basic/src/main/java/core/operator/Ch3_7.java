903d0f0b117a538faaa8a2da5d6ab016e7f6980a
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-25 17:02:52 +0800

    take(count, TimeUnit)

---
+++ rxjava/rxjava3/basic/src/main/java/core/operator/Ch3_7.java
@@ -0,0 +1,20 @@
+package core.operator;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+import static utils.ConcurrentUtil.sleep;
+
+public class Ch3_7 {
+    public static void main(String[] args) {
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("ss:SSS");
+        System.out.println(LocalDateTime.now().format(f));
+        Observable.interval(300, TimeUnit.MILLISECONDS)
+                .take(3, TimeUnit.SECONDS)
+                .subscribe(s -> System.out.println(LocalDateTime.now().format(f) + " RECEIVED: " + s));
+        sleep(5000);
+    }
+}
