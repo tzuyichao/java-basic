6387fb4e01846f36524d4ab64127e2d94a6d59f6
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-25 15:59:26 +0800

    skipWhile()

---
+++ rxjava/rxjava3/basic/src/main/java/core/operator/Ch3_2.java
@@ -0,0 +1,11 @@
+package core.operator;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class Ch3_2 {
+    public static void main(String[] args) {
+        Observable.range(1, 100)
+                .skipWhile(i -> i<95)
+                .subscribe(i -> System.out.println("RECEIVE: " + i));
+    }
+}
