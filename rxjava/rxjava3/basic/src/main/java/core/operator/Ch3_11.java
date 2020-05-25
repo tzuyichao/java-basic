3c92886b801c0bd0e50ffe8bdcfe48109135767d
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-25 17:12:46 +0800

    distinctUntilChange()

--- rxjava/rxjava3/basic/src/main/java/core/operator/Ch3_11.java
+++ rxjava/rxjava3/basic/src/main/java/core/operator/Ch3_11.java
@@ -7,5 +7,8 @@ public class Ch3_11 {
         Observable.just(1, 1, 1, 2, 2, 3, 3, 2, 1, 1)
                 .distinctUntilChanged()
                 .subscribe(i -> System.out.println("RECEIVE: " + i));
+        Observable.just("Alpha", "Beta", "Zeta", "Eta", "Gamma", "Delta")
+                .distinctUntilChanged(String::length)
+                .subscribe(s -> System.out.println("RECEIVE: " + s));
     }
 }
