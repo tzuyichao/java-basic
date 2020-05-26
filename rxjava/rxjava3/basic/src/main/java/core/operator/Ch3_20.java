c0e98738e94ab5db2a2fa720f2edbfa4f53df635
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-26 13:52:25 +0800

    lab

---
+++ rxjava/rxjava3/basic/src/main/java/core/operator/Ch3_20.java
@@ -0,0 +1,21 @@
+package core.operator;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class Ch3_20 {
+    public static void main(String[] args) {
+        Observable<Integer> source = Observable.just(6, 2, 5, 7, 1, 4, 9, 8, 3);
+        source.take(3)
+                .subscribe(System.out::println);
+        source.groupBy((k) -> {
+            System.out.println("key selector: " + k);
+            return k;
+        }, (v) -> {
+            System.out.println("value selector: " + v);
+            return v;
+        }, false, 3)
+        .subscribe(s -> {
+            System.out.println(s.getKey() + ":" + s.subscribe(i -> System.out.println(i.intValue())));
+        });
+    }
+}
