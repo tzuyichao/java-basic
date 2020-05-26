c3964562e21f19a8cd93dd7ee9011a94243d9e9a
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-26 11:28:37 +0800

    sorted()

---
+++ rxjava/rxjava3/basic/src/main/java/core/operator/Ch3_19.java
@@ -0,0 +1,13 @@
+package core.operator;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.Comparator;
+
+public class Ch3_19 {
+    public static void main(String[] args) {
+        Observable.just("Alpha", "Beta", "Gamma")
+                .sorted(Comparator.comparingInt(String::length))
+                .subscribe(System.out::println);
+    }
+}
