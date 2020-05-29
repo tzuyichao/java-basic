cc96e21e16161e8d127946dd08b73dd2d6203fec
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-29 15:27:26 +0800

    onErrorResumeWith()

---
+++ rxjava/rxjava3/basic/src/main/java/core/operator/Ch3_45.java
@@ -0,0 +1,13 @@
+package core.operator;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class Ch3_45 {
+    public static void main(String[] args) {
+        Observable.just(5, 2, 4, 0, 3)
+                .map(i -> 10 / i)
+                .onErrorResumeWith(Observable.just(-1).repeat(3))
+                .subscribe(i -> System.out.println("RECEIVED: " + i),
+                        e -> System.out.println("RECEIVED ERROR: " + e));
+    }
+}
