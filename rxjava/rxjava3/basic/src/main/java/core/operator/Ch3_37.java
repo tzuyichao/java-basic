c0768a515d5358e7bce6b9cbd42a4c938632cf24
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-29 14:28:23 +0800

    toMultiMap()

--- rxjava/rxjava3/basic/src/main/java/core/operator/Ch3_37.java
+++ rxjava/rxjava3/basic/src/main/java/core/operator/Ch3_37.java
@@ -5,6 +5,10 @@ import io.reactivex.rxjava3.core.Observable;
 public class Ch3_37 {
     public static void main(String[] args) {
         Observable.just("Alpha", "Gamma", "Beta")
+                .toMap(String::length)
+                .subscribe(s -> System.out.println("RECEIVED: " + s));
+
+        Observable.just("Alpha", "Gamma", "Beta")
                 .toMultimap(String::length)
                 .subscribe(s -> System.out.println("RECEIVED: " + s));
     }
