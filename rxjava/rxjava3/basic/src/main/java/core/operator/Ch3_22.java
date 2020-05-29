976049ad6b93862cadc42fe2edf3c4e3db4dcd5a
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-29 10:58:06 +0800

    count()

---
+++ rxjava/rxjava3/basic/src/main/java/core/operator/Ch3_22.java
@@ -0,0 +1,11 @@
+package core.operator;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class Ch3_22 {
+    public static void main(String[] args) {
+        Observable.just("Alpha", "Beta", "Gamma")
+                .count()
+                .subscribe(s -> System.out.println("RECEIVED: " + s));
+    }
+}
