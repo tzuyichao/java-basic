d5f412eb73ad7c61359a399625491acb8a44d397
TERENCE.CHAO <terence.chao@deltaww.com>
2020-05-29 13:34:50 +0800

    toList()

---
+++ rxjava/rxjava3/basic/src/main/java/core/operator/Ch3_31.java
@@ -0,0 +1,11 @@
+package core.operator;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class Ch3_31 {
+    public static void main(String[] args) {
+        Observable.range(1, 1000)
+                .toList(1000)
+                .subscribe(s -> System.out.println("RECEIVED: " + s));
+    }
+}
