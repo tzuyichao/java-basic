7041fc6fca69b96e574c018658eb5ea532275afe
TERENCE.CHAO <terence.chao@deltaww.com>
2020-06-01 14:15:53 +0800

    Mono first

---
+++ reactor/basic/src/main/java/core/MonoExample.java
@@ -0,0 +1,14 @@
+package core;
+
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+public class MonoExample {
+    public static void main(String[] args) {
+        Mono.just(1)
+                .map(i -> "foo: " + i)
+                .or(Mono.delay(Duration.ofMillis(100)).map(i -> "bar: " + i))
+                .subscribe(s -> System.out.println(System.currentTimeMillis() + ":" + s));
+    }
+}
