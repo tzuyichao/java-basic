1805d152fe0d4e5310005fc594c95983ca3b828f
TERENCE.CHAO <terence.chao@deltaww.com>
2020-06-01 11:59:08 +0800

    fist add

--- reactor/basic/src/main/java/core/FirstExample.java
+++ reactor/basic/src/main/java/core/FirstExample.java
@@ -4,7 +4,7 @@ import reactor.core.publisher.Flux;
 
 public class FirstExample {
     public static void main(String[] args) {
-        Flux<String> flux = Flux.just("A");
+        Flux<String> flux = Flux.just("Hello, World!");
         flux.map(s -> "foo: " + s);
         flux.subscribe(s -> System.out.println("RECEIVED: " + s));
     }
