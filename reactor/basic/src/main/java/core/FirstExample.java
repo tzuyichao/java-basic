459c5007afbba1aeff7dfc69efd6525ab29c34ee
TERENCE.CHAO <terence.chao@deltaww.com>
2020-06-01 13:25:42 +0800

    fist add

--- reactor/basic/src/main/java/core/FirstExample.java
+++ reactor/basic/src/main/java/core/FirstExample.java
@@ -2,10 +2,19 @@ package core;
 
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class FirstExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Flux<String> flux = Flux.just("Hello, World!");
         flux.map(s -> "foo: " + s);
         flux.subscribe(s -> System.out.println("RECEIVED: " + s));
+
+        Flux.interval(Duration.ofMillis(100))
+                .filter(l -> l < 10)
+                .subscribe(l -> System.out.println("RECEIVED: " + l));
+
+        TimeUnit.SECONDS.sleep(3);
     }
 }
