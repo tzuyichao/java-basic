464ea93f5cd04f71f92536ab7b056966f43215e1
TERENCE.CHAO <terence.chao@deltaww.com>
2020-06-02 13:56:45 +0800

    combineLatest()

--- rxjava/rxjava3/basic/src/main/java/core/reactiveoperator/Ch4_15.java
+++ rxjava/rxjava3/basic/src/main/java/core/reactiveoperator/Ch4_15.java
@@ -10,8 +10,8 @@ public class Ch4_15 {
     public static void main(String[] args) {
         Observable<Long> source1 = Observable.interval(300, TimeUnit.MILLISECONDS);
         Observable<Long> source2 = Observable.interval(1, TimeUnit.SECONDS);
-        Observable.combineLatest(source1, source2, (l1, l2) -> System.currentTimeMillis() + " - Source1: " + l1 + ",Source2: " + l2)
-                .subscribe(s -> System.out.println("RECEIVED: " + s));
+        Observable.combineLatest(source1, source2, (l1, l2) -> "Source1: " + l1 + ",Source2: " + l2)
+                .subscribe(s -> System.out.println(System.currentTimeMillis() + " - RECEIVED: " + s));
         sleep(4000);
     }
 }
