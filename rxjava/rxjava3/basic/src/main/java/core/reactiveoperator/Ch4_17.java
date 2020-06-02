61d348bd99fb87423272399ebb81585a688398d6
TERENCE.CHAO <terence.chao@deltaww.com>
2020-06-02 14:55:52 +0800

    groupBy()

---
+++ rxjava/rxjava3/basic/src/main/java/core/reactiveoperator/Ch4_17.java
@@ -0,0 +1,15 @@
+package core.reactiveoperator;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.observables.GroupedObservable;
+
+public class Ch4_17 {
+    public static void main(String[] args) {
+        Observable<String> source = Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon");
+        Observable<GroupedObservable<Integer, String>> byLength = source.groupBy(s -> s.length());
+        byLength.subscribe(o -> o.toList().subscribe(s -> System.out.println(s)));
+        byLength.flatMapSingle(grp -> grp.toList())
+                .subscribe(s -> System.out.println(s.toString()));
+
+    }
+}
