6ee79fa0360dbe36b40ad4b99020f464b2c3232c
TERENCE.CHAO <terence.chao@deltaww.com>
2020-06-03 10:48:03 +0800

    instanceof

---
+++ java-basic/src/main/java/basic/InstanceofLab.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class InstanceofLab {
+    public static void main(String[] args) {
+        Object a = new Object();
+        if(a instanceof Integer) {
+            System.out.println("a is Integer");
+        }
+    }
+}
