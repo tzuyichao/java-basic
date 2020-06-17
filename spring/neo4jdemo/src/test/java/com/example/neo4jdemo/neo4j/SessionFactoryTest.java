68221edfc56b542d51909c8c0891bb4f929e47b4
TERENCE.CHAO <terence.chao@deltaww.com>
2020-06-17 08:47:48 +0800

    sessionFactory lab

--- spring/neo4jdemo/src/test/java/com/example/neo4jdemo/neo4j/SessionFactoryTest.java
+++ spring/neo4jdemo/src/test/java/com/example/neo4jdemo/neo4j/SessionFactoryTest.java
@@ -1,10 +1,14 @@
 package com.example.neo4jdemo.neo4j;
 
 import org.junit.jupiter.api.Test;
+import org.neo4j.ogm.model.Result;
 import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -22,5 +26,29 @@ public class SessionFactoryTest {
     void test_sessionFactory_exist() {
         assertNotNull(sessionFactory);
         //sessionFactory.openSession().query()
+    }
+
+    @Test
+    void test_all_shortest_paths() {
+        Map<String, String> params = new HashMap<>();
+        params.put("person1", "Al Pacino");
+        params.put("person2", "Emil Eifrem");
+        Result result = sessionFactory.openSession().query("MATCH (p1:Person { name: $person1 }),(p2:Person { name: $person2 }), p = allShortestPaths((p1)-[*]-(p2)) RETURN p", params);
+        assertNotNull(result);
+        for(Map<String, Object> record : result.queryResults()) {
+            System.out.println(record);
+        }
+    }
+
+    @Test
+    void test_all_paths() {
+        Map<String, String> params = new HashMap<>();
+        params.put("person1", "Al Pacino");
+        params.put("person2", "Emil Eifrem");
+        Result result = sessionFactory.openSession().query("MATCH (p1:Person { name: $person1 }),(p2:Person { name: $person2 }), p = ((p1)-[*]-(p2)) RETURN p limit 3", params);
+        assertNotNull(result);
+        for(Map<String, Object> record : result.queryResults()) {
+            System.out.println(record);
+        }
     }
 }
