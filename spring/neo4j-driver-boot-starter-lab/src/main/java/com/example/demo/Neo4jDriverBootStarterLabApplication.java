0946d7b68e914f57bb044a4a2b456ce22aadcdba
TERENCE.CHAO <terence.chao@deltaww.com>
2020-06-19 10:12:57 +0800

    first lab from official web site

---
+++ spring/neo4j-driver-boot-starter-lab/src/main/java/com/example/demo/Neo4jDriverBootStarterLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jDriverBootStarterLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jDriverBootStarterLabApplication.class, args);
+	}
+
+}
