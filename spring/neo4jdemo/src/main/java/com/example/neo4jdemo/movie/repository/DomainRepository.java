197b152f3c06e5272f9ffb65fbf4379168edbd22
TERENCE.CHAO <terence.chao@deltaww.com>
2020-07-03 08:38:22 +0800

    test parameter

--- spring/neo4jdemo/src/main/java/com/example/neo4jdemo/movie/repository/DomainRepository.java
+++ spring/neo4jdemo/src/main/java/com/example/neo4jdemo/movie/repository/DomainRepository.java
@@ -3,17 +3,22 @@ package com.example.neo4jdemo.movie.repository;
 import com.example.neo4jdemo.movie.model.Domain;
 import com.example.neo4jdemo.movie.model.DomainStatus;
 import org.neo4j.ogm.model.Result;
+import org.springframework.data.neo4j.annotation.Depth;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DomainRepository extends Neo4jRepository<Domain, Long> {
     @Query("Match (d:Domain {name: $name}) where d.status <> 'DELETED' return d")
     Collection<Domain> findByName(@Param("name") String name);
+
+    List<Domain> findByNameAndStatus(String name, DomainStatus status, @Depth int depth);
 
     @Query("Match (s:Domain)-[:GLOSSARY_HIERARCHY]->(r:Domain) where id(s)=$id and r.status <> 'DELETED' return r limit 1")
     Domain parentDomain(@Param("id") Long id);
@@ -38,4 +43,7 @@ public interface DomainRepository extends Neo4jRepository<Domain, Long> {
 
     @Query("Match (d:Domain)-[r:GLOSSARY_HIERARCHY]-(c:Domain) where id(d) = $id delete r")
     Result deleteOtherDomainRelationship(@Param("id") Long id);
+
+    @Query("Match (d:Domain) where d.name=$domain.name set d.desc = $domain.desc return d")
+    Optional<Domain> searchTest(@Param("domain") Domain domain);
 }
