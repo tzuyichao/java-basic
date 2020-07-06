2d733569c5dcd165517ccf8b15395e806bf857b7
TzuYi Chao <tzuyichao@mail2000.com.tw>
2020-07-06 23:12:06 +0800

    root

--- spring/neo4jdemo/src/test/java/com/example/neo4jdemo/movie/controller/DomainControllerTest.java
+++ spring/neo4jdemo/src/test/java/com/example/neo4jdemo/movie/controller/DomainControllerTest.java
@@ -1,4 +1,28 @@
 package com.example.neo4jdemo.movie.controller;
 
+import com.example.neo4jdemo.movie.model.Domain;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collection;
+import java.util.Map;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DomainControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testRoot() {
+        ResponseEntity<Collection> result = restTemplate.getForEntity("/domain/root", Collection.class);
+        System.out.println(result.getBody());
+    }
+
 }
