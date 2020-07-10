eb345ace88260b850436760aa4c80a14258e75e7
TERENCE.CHAO <terence.chao@deltaww.com>
2020-07-10 13:49:40 +0800

    conversion

---
+++ spring/conversion-demo/src/main/java/com/example/conversiondemo/ConversionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.conversiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversionDemoApplication.class, args);
+	}
+
+}
