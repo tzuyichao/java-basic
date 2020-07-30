a97cce526ee5f221ac8ce235403acfd4f7f61848
TERENCE.CHAO <terence.chao@deltaww.com>
2020-07-30 09:27:59 +0800

    first add

---
+++ javafx/hello/src/main/java/hello/HelloFX.java
@@ -0,0 +1,24 @@
+package hello;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloFX extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label label = new Label("Hello, JavaFX " + javafxVersion + ", running on java " + javaVersion);
+        Scene scene = new Scene(new StackPane(label), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
