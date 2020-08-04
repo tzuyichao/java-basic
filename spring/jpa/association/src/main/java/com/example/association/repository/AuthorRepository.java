f51581921d1d97f22d6272093783cfd4b01649b6
TERENCE.CHAO <terence.chao@deltaww.com>
2020-08-04 13:31:56 +0800

    AuthorRepository unit test

--- spring/jpa/association/src/main/java/com/example/association/repository/AuthorRepository.java
+++ spring/jpa/association/src/main/java/com/example/association/repository/AuthorRepository.java
@@ -3,5 +3,8 @@ package com.example.association.repository;
 import com.example.association.model.Author;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface AuthorRepository extends CrudRepository<Author, Long> {
+    Optional<Author> findByName(String name);
 }
---
+++ spring/jpa/association/src/test/java/com/example/association/repository/AuthorRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.example.association.repository;
+
+import com.example.association.model.Author;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class AuthorRepositoryTest {
+    @Autowired AuthorRepository authorRepository;
+
+    @Test
+    void inject_component_should_success() {
+        assertThat(authorRepository).isNotNull();
+    }
+
+    @Test
+    void givenAuthor_whenSearchByName() {
+        Author author = new Author();
+        author.setName("Enrich Gamma");
+        authorRepository.save(author);
+
+        assertThat(authorRepository.findByName("Enrich Gamma")).isPresent();
+    }
+}
