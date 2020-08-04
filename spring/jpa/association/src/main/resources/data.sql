07e7c701d309b066e6eb4c48db01c52b42d2e09f
TERENCE.CHAO <terence.chao@deltaww.com>
2020-08-04 16:52:12 +0800

    singer example from pro spring 5

--- spring/jpa/association/src/main/resources/data.sql
+++ spring/jpa/association/src/main/resources/data.sql
@@ -20,3 +20,39 @@ CREATE TABLE book (
   modified TIMESTAMP,
   FOREIGN KEY (author_id) REFERENCES author(id)
 );
+
+DROP TABLE IF EXISTS singer_instrument;
+DROP TABLE IF EXISTS instrument;
+DROP TABLE IF EXISTS album;
+DROP TABLE IF EXISTS singer;
+
+CREATE TABLE singer (
+  id int auto_increment primary key,
+  first_name varchar(60) not null,
+  last_name varchar(40) not null,
+  birth_date date,
+  version int not null default 0,
+  unique uq_singer_1 (first_name, last_name)
+);
+
+CREATE TABLE album (
+  id int auto_increment primary key,
+  singer_id int not null,
+  title varchar(100) not null,
+  release_date date,
+  version int not null default 0,
+  unique uq_singer_album_1 (singer_id, title),
+  FOREIGN KEY (singer_id) REFERENCES singer(id)
+);
+
+CREATE TABLE instrument (
+  instrument_id varchar(20) primary key
+);
+
+CREATE TABLE singer_instrument (
+  singer_id int not null,
+  instrument_id varchar(20) not null,
+  primary key (singer_id, instrument_id),
+  FOREIGN KEY (singer_id) REFERENCES singer(id),
+  FOREIGN KEY (instrument_id) REFERENCES instrument(instrument_id)
+);
