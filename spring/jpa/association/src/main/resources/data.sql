cf6b784389a7ae303b122a3aab255e4cbee0ed7c
TERENCE.CHAO <terence.chao@deltaww.com>
2020-08-06 13:42:35 +0800

    data

--- spring/jpa/association/src/main/resources/data.sql
+++ spring/jpa/association/src/main/resources/data.sql
@@ -56,3 +56,21 @@ CREATE TABLE singer_instrument (
   FOREIGN KEY (singer_id) REFERENCES singer(id),
   FOREIGN KEY (instrument_id) REFERENCES instrument(instrument_id)
 );
+
+insert into singer (first_name, last_name, birth_date) values ('John', 'Mayer', '1977-10-16');
+insert into singer (first_name, last_name, birth_date) values ('Eric', 'Clapton', '1945-03-30');
+insert into singer (first_name, last_name, birth_date) values ('John', 'Butler', '1975-04-01');
+
+insert into album (singer_id, title, release_date) values (1, 'The Search For Everything', '2017-01-20');
+insert into album (singer_id, title, release_date) values (1, 'Battle Studies', '2009-11-17');
+insert into album (singer_id, title, release_date) values (2, 'From The Cradle ', '1994-09-13');
+
+insert into instrument (instrument_id) values ('Guitar');
+insert into instrument (instrument_id) values ('Piano');
+insert into instrument (instrument_id) values ('Voice');
+insert into instrument (instrument_id) values ('Drums');
+insert into instrument (instrument_id) values ('Synthesizer');
+
+insert into singer_instrument(singer_id, instrument_id) values (1, 'Guitar');
+insert into singer_instrument(singer_id, instrument_id) values (1, 'Piano');
+insert into singer_instrument(singer_id, instrument_id) values (2, 'Guitar');
