86f3dc0edb39a787375cc48b18cd60b3eee569d6
TERENCE.CHAO <terence.chao@deltaww.com>
2020-08-14 16:42:20 +0800

    test

--- spring/neo4jdemo/src/main/java/com/example/neo4jdemo/movie/repository/LineageRepository.java
+++ spring/neo4jdemo/src/main/java/com/example/neo4jdemo/movie/repository/LineageRepository.java
@@ -1,9 +1,17 @@
 package com.example.neo4jdemo.movie.repository;
 
+import com.example.neo4jdemo.movie.model.ColumnUnit;
 import com.example.neo4jdemo.movie.model.Lineage;
+import org.neo4j.ogm.model.Result;
+import org.springframework.data.neo4j.annotation.Depth;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface LineageRepository extends Neo4jRepository<Lineage, Long> {
+    @Query("MATCH (s:ColumnUnit)-[r:LINEAGE_LINK]->(t:ColumnUnit) WHERE id(s)=$sourceId return s, r, t")
+    Lineage[] findBySource(Long sourceId);
 }
--- spring/neo4jdemo/src/test/java/com/example/neo4jdemo/movie/repository/LineageRepositoryTest.java
+++ spring/neo4jdemo/src/test/java/com/example/neo4jdemo/movie/repository/LineageRepositoryTest.java
@@ -1,29 +1,33 @@
 package com.example.neo4jdemo.movie.repository;
 
-import com.example.neo4jdemo.movie.model.CatalogUnit;
-import com.example.neo4jdemo.movie.model.ColumnUnit;
-import com.example.neo4jdemo.movie.model.Lineage;
-import com.example.neo4jdemo.movie.model.SystemUnit;
+import com.example.neo4jdemo.movie.model.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.neo4j.ogm.model.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.Collection;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @Testcontainers
 @DataNeo4jTest
 @ImportAutoConfiguration(classes={com.example.neo4jdemo.movie.config.DatabaseConfig.class})
 public class LineageRepositoryTest {
     private LineageRepository lineageRepository;
+    private ColumnUnitRepository columnUnitRepository;
 
     @Autowired
-    public LineageRepositoryTest(LineageRepository lineageRepository) {
+    public LineageRepositoryTest(LineageRepository lineageRepository, ColumnUnitRepository columnUnitRepository) {
         this.lineageRepository = lineageRepository;
+        this.columnUnitRepository = columnUnitRepository;
     }
-
     @Test
     public void test_non_columnunit_add_lineage_failed() {
         Assertions.assertThrows(ClassCastException.class, () -> {
@@ -59,5 +63,81 @@ public class LineageRepositoryTest {
         assertNotNull(source.getId());
         assertNotNull(target.getId());
         assertNotNull(lineage.getId());
+    }
+
+    @Test
+    void test_delete_model_behavior() {
+        ColumnUnit source = new ColumnUnit();
+        source.setName("source1");
+        source.setStatus(ResourceStatus.PUBLISHED);
+        ColumnUnit target = new ColumnUnit();
+        target.setName("target1");
+        target.setStatus(ResourceStatus.PUBLISHED);
+        Lineage lineage = new Lineage();
+        lineage.setSource(source);
+        lineage.setTarget(target);
+        lineageRepository.save(lineage, 1);
+        System.out.println("source: " + source.getId());
+        System.out.println("target: " + target.getId());
+        System.out.println("lineage: " + lineage.getId());
+        Long sourceId = source.getId();
+        Long targetId = target.getId();
+        Long lineageId = lineage.getId();
+
+        lineageRepository.delete(lineage);
+
+        Optional<ColumnUnit> sourceOptional = columnUnitRepository.findById(sourceId, 1);
+        Optional<ColumnUnit> targetOptional = columnUnitRepository.findById(targetId, 1);
+        if(sourceOptional.isPresent()) {
+            System.out.println("source got");
+        } else {
+            fail("source deleted too");
+        }
+        if(targetOptional.isPresent()) {
+            System.out.println("target got");
+        } else {
+            fail("target deleted too");
+        }
+        Optional<Lineage> lineageOptional = lineageRepository.findById(lineageId);
+        if(lineageOptional.isPresent()) {
+            System.out.println(">>>>>>> Big problem");
+            fail("Big Problem");
+        }
+    }
+
+    @Test
+    void test_find_source_id_with_depth() {
+        ColumnUnit source = new ColumnUnit();
+        source.setName("source1");
+        source.setStatus(ResourceStatus.PUBLISHED);
+        ColumnUnit target = new ColumnUnit();
+        target.setName("target1");
+        target.setStatus(ResourceStatus.PUBLISHED);
+        Lineage lineage = new Lineage();
+        lineage.setSource(source);
+        lineage.setTarget(target);
+        lineageRepository.save(lineage, 1);
+        System.out.println("source: " + source.getId());
+        System.out.println("target: " + target.getId());
+        System.out.println("lineage: " + lineage.getId());
+        Long sourceId = source.getId();
+        Long targetId = target.getId();
+        Long lineageId = lineage.getId();
+
+        Lineage[] result = lineageRepository.findBySource(sourceId);
+        for(Lineage lineage1 : result) {
+            System.out.println(lineage1);
+            System.out.println(lineage1.getSource().getName());
+            System.out.println(lineage1.getTarget().getName());
+        }
+//        assertThat(lineages)
+//                .isNotNull()
+//                .satisfies(lineages1 -> {
+//                    assertThat(lineages1.length).isEqualTo(1);
+//                    for(Lineage lineage1 : lineages1) {
+//                        assertThat(lineage1.getSource()).isNotNull();
+//                        assertThat(lineage1.getTarget()).isNotNull();
+//                    }
+//                });
     }
 }
