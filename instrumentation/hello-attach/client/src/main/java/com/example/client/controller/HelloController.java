4f02a5ead82b9001dceb647aae52a2d482187220
TERENCE.CHAO <terence.chao@deltaww.com>
2020-09-11 16:24:36 +0800

    first add

---
+++ instrumentation/hello-attach/client/src/main/java/com/example/client/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.client.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping
+    public String hello() {
+        return "Greeting.";
+    }
+}
