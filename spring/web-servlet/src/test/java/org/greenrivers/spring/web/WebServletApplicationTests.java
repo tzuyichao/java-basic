84d8460fed11dc8b5b553bd69cb1279135c1053b
TzuYi Chao <tzuyichao@mail2000.com.tw>
2020-09-12 10:39:02 +0800

    test

---
+++ spring/web-servlet/src/test/java/org/greenrivers/spring/web/WebServletApplicationTests.java
@@ -0,0 +1,13 @@
+package org.greenrivers.spring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
