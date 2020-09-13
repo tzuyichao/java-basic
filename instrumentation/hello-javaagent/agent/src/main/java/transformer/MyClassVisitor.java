fac10d2586a04950d48b3c5164205b3a6d6aad56
TzuYi Chao <tzuyichao@mail2000.com.tw>
2020-09-13 16:07:30 +0800

    test

--- instrumentation/hello-javaagent/agent/src/main/java/transformer/MyClassVisitor.java
+++ instrumentation/hello-javaagent/agent/src/main/java/transformer/MyClassVisitor.java
@@ -3,9 +3,16 @@ package transformer;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
 
+import java.util.Collections;
+import java.util.Set;
+
 import static org.objectweb.asm.Opcodes.ASM8;
 
 public class MyClassVisitor extends ClassVisitor {
+    public static final String NAME_CONSTRUCTOR = "<init>";
+    public static final String NAME_MAIN = "main";
+    public static final Set WhiteList = Collections.unmodifiableSet(Set.of(NAME_CONSTRUCTOR, NAME_MAIN));
+
     public MyClassVisitor(ClassVisitor classVisitor) {
         super(ASM8, classVisitor);
     }
@@ -13,7 +20,7 @@ public class MyClassVisitor extends ClassVisitor {
     @Override
     public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
         MethodVisitor methodVisitor = super.visitMethod(access, name, descriptor, signature, exceptions);
-        if("<init>".equals(name)) {
+        if(WhiteList.contains(name)) {
             return methodVisitor;
         }
         return new MyMethodVisitor(ASM8, methodVisitor, access, name, descriptor);
