cd4a742434108fd15bb6386a8f28a2f3ecd8641e
TERENCE.CHAO <terence.chao@deltaww.com>
2020-09-14 12:38:23 +0800

    add AttachProvider lab

--- instrumentation/hello-attach/attach/src/main/java/attach/ListAttachProvider.java
+++ instrumentation/hello-attach/attach/src/main/java/attach/ListAttachProvider.java
@@ -1,10 +1,12 @@
 package attach;
 
 import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
 import com.sun.tools.attach.VirtualMachineDescriptor;
 import com.sun.tools.attach.spi.AttachProvider;
 
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
 import java.util.List;
 
 public class ListAttachProvider {
@@ -15,6 +17,8 @@ public class ListAttachProvider {
             try {
                 // this process should not access able
                 attachProvider.attachVirtualMachine("100");
+                VirtualMachine virtualMachine = attachProvider.attachVirtualMachine("17104");
+                System.out.println(virtualMachine);
             } catch(IOException | AttachNotSupportedException e) {
                 e.printStackTrace();
             }
@@ -22,6 +26,8 @@ public class ListAttachProvider {
             for(VirtualMachineDescriptor virtualMachineDescriptor : virtualMachineDescriptorList) {
                 System.out.println(virtualMachineDescriptor.toString());
             }
+
+            System.out.println(ManagementFactory.getRuntimeMXBean().getName());
         }
     }
 }
