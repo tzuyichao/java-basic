5b94a134524daaf9ab38201b7530d5e51e2c48be
TzuYi Chao <tzuyichao@mail2000.com.tw>
2020-10-04 16:32:50 +0800

    web 01

---
+++ spring/web01/src/main/java/demo/config/AppInitializer.java
@@ -0,0 +1,20 @@
+package demo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { WebMvcConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
