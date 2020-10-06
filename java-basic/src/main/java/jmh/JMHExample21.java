4753ab2bd4f15628641e287516de95081df4ebfc
TERENCE.CHAO <terence.chao@deltaww.com>
2020-10-06 13:55:56 +0800

    Profiler

---
+++ java-basic/src/main/java/jmh/JMHExample21.java
@@ -0,0 +1,52 @@
+package jmh;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.profile.StackProfiler;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@Fork(1)
+@Warmup(iterations = 5)
+@Measurement(iterations = 5)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@State(Scope.Group)
+public class JMHExample21 {
+    private BlockingQueue<Integer> queue;
+    private final static int VALUE = Integer.MAX_VALUE;
+
+    @Setup
+    public void init() {
+        this.queue = new ArrayBlockingQueue<>(10);
+    }
+
+    @GroupThreads(5)
+    @Group("blockingQueue")
+    @Benchmark
+    public void put() throws InterruptedException {
+        this.queue.put(VALUE);
+    }
+
+    @GroupThreads(5)
+    @Group("blockingQueue")
+    @Benchmark
+    public void take() throws InterruptedException {
+        this.queue.take();
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        final Options options = new OptionsBuilder()
+                .include(JMHExample21.class.getSimpleName())
+                .timeout(TimeValue.seconds(10))
+                .addProfiler(StackProfiler.class)
+                .build();
+        new Runner(options).run();
+    }
+}
