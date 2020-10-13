7a3e674421757dddffef2d04541e27f1339d8b2e
TERENCE.CHAO <terence.chao@deltaww.com>
2020-10-13 17:18:28 +0800

    remove unused import

--- spring/security/auth-server-jwt/src/main/java/com/example/security/jwt/SpringSecurityConfig.java
+++ spring/security/auth-server-jwt/src/main/java/com/example/security/jwt/SpringSecurityConfig.java
@@ -1,15 +1,10 @@
 package com.example.security.jwt;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Log4j2
 @Configuration
