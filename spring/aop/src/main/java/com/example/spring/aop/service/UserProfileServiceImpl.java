6cfb9e1f713dc465b9ad686e24c080c0225b5a31
TERENCE.CHAO <terence.chao@deltaww.com>
2020-12-18 17:11:12 +0800

    for check service inject aspect

--- spring/aop/src/main/java/com/example/spring/aop/service/UserProfileServiceImpl.java
+++ spring/aop/src/main/java/com/example/spring/aop/service/UserProfileServiceImpl.java
@@ -1,10 +1,18 @@
 package com.example.spring.aop.service;
 
 import com.example.spring.aop.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserProfileServiceImpl implements UserProfileService {
+    private static final Logger log = LoggerFactory.getLogger(UserProfileServiceImpl.class);
+
+    public UserProfileServiceImpl() {
+        log.debug("CONSTRUCTOR Called.");
+    }
+
     @Override
     public User getUser(String accessToken) {
         return null;
