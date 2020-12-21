fcbdd92c071ebfc1c7876b3f88d6955a454617f0
TERENCE.CHAO <terence.chao@deltaww.com>
2020-12-21 14:51:56 +0800

    echo without count connection number

--- vertx/vertx-echo/src/main/java/com/example/demo/MainVerticle.java
+++ vertx/vertx-echo/src/main/java/com/example/demo/MainVerticle.java
@@ -2,12 +2,26 @@ package com.example.demo;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpConnection;
+import io.vertx.core.net.NetSocket;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
+    vertx.createNetServer()
+      .connectHandler(this::handleNewConnection)
+      .listen(3000, netServerAsyncResult -> {
+        if(netServerAsyncResult.succeeded()) {
+          System.out.println("Net Server started on port 3000");
+        } else {
+          System.err.println(netServerAsyncResult.cause());
+        }
+      });
+
+    vertx.createHttpServer()
+      .connectionHandler(this::handleNewHttpConnection)
+      .requestHandler(req -> {
       req.response()
         .putHeader("content-type", "text/plain")
         .end("Hello from Vert.x!");
@@ -17,6 +31,19 @@ public class MainVerticle extends AbstractVerticle {
         System.out.println("HTTP server started on port 8888");
       } else {
         startPromise.fail(http.cause());
+      }
+    });
+  }
+
+  private void handleNewHttpConnection(HttpConnection httpConnection) {
+    System.out.println(httpConnection.toString());
+  }
+
+  private void handleNewConnection(NetSocket netSocket) {
+    netSocket.handler(buffer -> {
+      netSocket.write(buffer);
+      if(buffer.toString().endsWith("/quit\n")) {
+        netSocket.close();
       }
     });
   }
