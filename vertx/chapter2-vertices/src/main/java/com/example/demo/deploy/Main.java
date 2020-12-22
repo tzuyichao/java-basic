9ade603938c7c97126f34e227a3971b4a5a630f4
TERENCE.CHAO <terence.chao@deltaww.com>
2020-12-22 14:48:17 +0800

    deploy verticle

---
+++ vertx/chapter2-vertices/src/main/java/com/example/demo/deploy/Main.java
@@ -0,0 +1,10 @@
+package com.example.demo.deploy;
+
+import io.vertx.core.Vertx;
+
+public class Main {
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new Deployer());
+  }
+}
