d581a586f7620e5ccff4e110c5571bbe4c8a12f1
TERENCE.CHAO <terence.chao@deltaww.com>
2020-12-24 10:25:21 +0800

    add for sse

--- vertx/event-bus/src/main/java/heat/Main.java
+++ vertx/event-bus/src/main/java/heat/Main.java
@@ -1,11 +1,16 @@
 package heat;
 
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 
 public class Main {
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
-        vertx.deployVerticle("heat.HeatSensor");
+
+        DeploymentOptions heatSensorDeploymentOptions = new DeploymentOptions()
+                .setInstances(10);
+
+        vertx.deployVerticle("heat.HeatSensor", heatSensorDeploymentOptions);
         vertx.deployVerticle("heat.Listener");
         vertx.deployVerticle("heat.SensorData");
         vertx.deployVerticle("heat.HttpServer");
