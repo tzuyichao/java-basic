a248bb01a76afe33f21d6e9992d48572b520cced
TERENCE.CHAO <terence.chao@deltaww.com>
2020-12-24 16:57:11 +0800

    draft

--- vertx/distributed-event-bus/src/main/java/com/example/demo/heat/SecondInstance.java
+++ vertx/distributed-event-bus/src/main/java/com/example/demo/heat/SecondInstance.java
@@ -2,10 +2,8 @@ package com.example.demo.heat;
 
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-public class FirstInstance {
+public class SecondInstance {
   private static DeploymentOptions getHeatSensorDeploymentOptions() {
     return new DeploymentOptions().setInstances(4);
   }
