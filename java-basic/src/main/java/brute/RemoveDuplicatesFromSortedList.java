c19630ebce16dae0269feb90fb29101319ec1dbe
tzuyichao <tzuyichao@mail2000.com.tw>
2021-01-25 09:59:26 +0800

    first submission

--- java-basic/src/main/java/brute/RemoveDuplicatesFromSortedList.java
+++ java-basic/src/main/java/brute/RemoveDuplicatesFromSortedList.java
@@ -5,6 +5,21 @@ package brute;
  */
 public class RemoveDuplicatesFromSortedList {
     public ListNode deleteDuplicates(ListNode head) {
-        return null;
+        if(null == head) {
+            return null;
+        }
+        int current = head.val;
+        ListNode result = new ListNode(head.val);
+        ListNode currentResultNode = result;
+        ListNode previousNode = head;
+        while(previousNode.next != null) {
+            if(current != previousNode.next.val) {
+                currentResultNode.next = new ListNode(previousNode.next.val);
+                currentResultNode = currentResultNode.next;
+            }
+            previousNode = previousNode.next;
+            current = previousNode.val;
+        }
+        return result;
     }
 }
