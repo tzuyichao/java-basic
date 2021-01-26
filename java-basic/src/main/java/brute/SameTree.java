d043d080bd9945c852824e26384c14a3eafbce32
tzuyichao <tzuyichao@mail2000.com.tw>
2021-01-26 10:10:23 +0800

    1 submission

--- java-basic/src/main/java/brute/SameTree.java
+++ java-basic/src/main/java/brute/SameTree.java
@@ -4,7 +4,29 @@ package brute;
  * = 100
  */
 public class SameTree {
+    private boolean result = true;
+
+    private void walkAndCompare(TreeNode p, TreeNode q) {
+        if(p == null && q == null) {
+            return;
+        } else if((p == null && q != null) || (p != null && q == null)) {
+            result = false;
+            return;
+        } else {
+            if (p.val != q.val) {
+                result = false;
+                return;
+            }
+            walkAndCompare(p.right, q.right);
+            walkAndCompare(p.left, q.left);
+        }
+    }
+
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return false;
+        if(p == q) {
+            return true;
+        }
+        walkAndCompare(p, q);
+        return result;
     }
 }
--- java-basic/src/test/java/brute/SameTreeTest.java
+++ java-basic/src/test/java/brute/SameTreeTest.java
@@ -1,4 +1,44 @@
 package brute;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class SameTreeTest {
+    private SameTree sameTree;
+
+    @Before
+    public void init() {
+        sameTree = new SameTree();
+    }
+
+    @Test
+    public void simple1() {
+        TreeNode p = null;
+        TreeNode q = null;
+        assertTrue(sameTree.isSameTree(p, q));
+    }
+
+    @Test
+    public void simple2() {
+        TreeNode p = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        TreeNode q = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        assertTrue(sameTree.isSameTree(p, q));
+    }
+
+    @Test
+    public void simple3() {
+        TreeNode p = new TreeNode(1, null, new TreeNode(3));
+        TreeNode q = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        assertFalse(sameTree.isSameTree(p, q));
+    }
+
+    @Test
+    public void simple4() {
+        TreeNode p = new TreeNode(1, new TreeNode(2, new TreeNode(4), null), new TreeNode(3));
+        TreeNode q = new TreeNode(1, new TreeNode(2, new TreeNode(4), null), new TreeNode(3));
+        assertTrue(sameTree.isSameTree(p, q));
+    }
 }
