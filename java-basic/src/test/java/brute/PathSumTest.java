c631f0def9a3fbab577e5e70849b8d16909f50a3
tzuyichao <tzuyichao@mail2000.com.tw>
2021-01-27 17:15:58 +0800

    Runtime Error 1 Wrong 3

--- java-basic/src/test/java/brute/PathSumTest.java
+++ java-basic/src/test/java/brute/PathSumTest.java
@@ -1,6 +1,5 @@
 package brute;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -9,12 +8,6 @@ import java.io.InputStream;
 import static org.junit.Assert.assertEquals;
 
 public class PathSumTest {
-    PathSum solver;
-
-    @Before
-    public void init() {
-        solver = new PathSum();
-    }
 
     @Test
     public void simple1() throws IOException {
@@ -24,20 +17,24 @@ public class PathSumTest {
         String[] all = content.split("\\r\\n");
 
         for(int i=0; i<all.length/3; i++) {
+            PathSum solver = new PathSum();
             String treeString = all[i*3];
             String sumString = all[i*3+1];
             String expectString = all[i*3+2];
-            String[] treeArray = treeString.trim().replace("[", "").replace("]", "").split(",");
-            Integer[] data = new Integer[treeArray.length];
-            for(int idx=0; idx<treeArray.length; idx++) {
-                if(treeArray[idx].equals("null")) {
-                    data[idx] = null;
-                } else {
-                    data[idx] = Integer.valueOf(treeArray[idx]);
+            Integer[] data = null;
+            if(!treeString.trim().equals("[]")) {
+                String[] treeArray = treeString.trim().replace("[", "").replace("]", "").split(",");
+                data = new Integer[treeArray.length];
+                for (int idx = 0; idx < treeArray.length; idx++) {
+                    if (treeArray[idx].equals("null")) {
+                        data[idx] = null;
+                    } else {
+                        data[idx] = Integer.valueOf(treeArray[idx]);
+                    }
                 }
             }
             TreeNode tree = TreeNodeHelper.make(data);
-
+            System.out.println(treeString);
             assertEquals(Boolean.parseBoolean(expectString.trim()), solver.hasPathSum(tree, Integer.parseInt(sumString)));
         }
     }
