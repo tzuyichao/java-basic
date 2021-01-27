d79ae8e1ac43c50ea88d8e9357f39099d49f1d6d
tzuyichao <tzuyichao@mail2000.com.tw>
2021-01-27 10:23:51 +0800

    又沒人說Linked list內容不能更改

--- java-basic/src/main/java/brute/LinkedListCycle.java
+++ java-basic/src/main/java/brute/LinkedListCycle.java
@@ -1,28 +1,24 @@
 package brute;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * = 141
  */
 public class LinkedListCycle {
-    Map<ListNode, Boolean> check = new HashMap();
+    int visited = -10000000;
 
     private boolean solve(ListNode node) {
         if(null == node || null == node.next) {
             return false;
         }
-        check.put(node, true);
-        if(check.containsKey(node.next)) {
+        if(node.val == visited) {
             return true;
         } else {
+            node.val = visited;
             return solve(node.next);
         }
     }
 
     public boolean hasCycle(ListNode head) {
-        check.put(head, true);
         return solve(head);
     }
 }
