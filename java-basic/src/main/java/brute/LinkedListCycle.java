7084be0ca0dff77d574bf1ddefcf96b79d7d42e9
tzuyichao <tzuyichao@mail2000.com.tw>
2021-01-27 10:14:29 +0800

    brute force 1

--- java-basic/src/main/java/brute/LinkedListCycle.java
+++ java-basic/src/main/java/brute/LinkedListCycle.java
@@ -1,10 +1,28 @@
 package brute;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * = 141
  */
 public class LinkedListCycle {
+    Map<ListNode, Boolean> check = new HashMap();
+
+    private boolean solve(ListNode node) {
+        if(null == node || null == node.next) {
+            return false;
+        }
+        check.put(node, true);
+        if(check.containsKey(node.next)) {
+            return true;
+        } else {
+            return solve(node.next);
+        }
+    }
+
     public boolean hasCycle(ListNode head) {
-        return false;
+        check.put(head, true);
+        return solve(head);
     }
 }
