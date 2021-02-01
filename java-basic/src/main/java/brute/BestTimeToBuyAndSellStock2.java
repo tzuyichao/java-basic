3fe6cdd018ebe68fa04b0da19f98d928f159a8b2
tzuyichao <tzuyichao@mail2000.com.tw>
2021-02-01 17:35:57 +0800

    殼

---
+++ java-basic/src/main/java/brute/BestTimeToBuyAndSellStock2.java
@@ -0,0 +1,7 @@
+package brute;
+
+public class BestTimeToBuyAndSellStock2 {
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+}
---
+++ java-basic/src/test/java/brute/BestTimeToBuyAndSellStock2Test.java
@@ -0,0 +1,33 @@
+package brute;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BestTimeToBuyAndSellStock2Test {
+    BestTimeToBuyAndSellStock2 solver;
+
+    @Before
+    public void init() {
+        solver = new BestTimeToBuyAndSellStock2();
+    }
+    
+    @Test
+    public void simple1() {
+        int[] prices = new int[] {7,1,5,3,6,4};
+        assertEquals(7, solver.maxProfit(prices));
+    }
+
+    @Test
+    public void simple2() {
+        int[] prices = new int[] {1,2,3,4,5};
+        assertEquals(4, solver.maxProfit(prices));
+    }
+
+    @Test
+    public void simple3() {
+        int[] prices = new int[] {7,6,4,3,1};
+        assertEquals(0, solver.maxProfit(prices));
+    }
+}
