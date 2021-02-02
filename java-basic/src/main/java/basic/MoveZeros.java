dfc884f102f4771acfdb9776042a32b928d7ce53
tzuyichao <tzuyichao@mail2000.com.tw>
2021-02-02 17:16:48 +0800

    暴力

---
+++ java-basic/src/main/java/basic/MoveZeros.java
@@ -0,0 +1,23 @@
+package basic;
+
+/**
+ * = 283
+ */
+public class MoveZeros {
+    private void swap(int[] nums, int sourceIndex, int targetIndex) {
+        int temp = nums[targetIndex];
+        nums[targetIndex] = nums[sourceIndex];
+        nums[sourceIndex] = temp;
+    }
+
+    public void moveZeroes(int[] nums) {
+        int move = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == 0) {
+                move += 1;
+            } else {
+                swap(nums, i, i-move);
+            }
+        }
+    }
+}
