74954b4526c4fa0bc4735436a9ad385414470a0c
tzuyichao <tzuyichao@mail2000.com.tw>
2021-02-05 16:03:37 +0800

    暴力

--- java-basic/src/main/java/brute/AddTwoNumbers.java
+++ java-basic/src/main/java/brute/AddTwoNumbers.java
@@ -1,10 +1,36 @@
 package brute;
 
+import java.math.BigDecimal;
+
 /**
  * = 2
  */
 public class AddTwoNumbers {
+    public ListNode BigDecimalToListNode(BigDecimal val) {
+        String[] values = String.valueOf(val).split("");
+        ListNode result = new ListNode(Integer.parseInt(values[values.length-1]));
+        ListNode current = result;
+        for(int i=values.length-2; i>=0; i--) {
+            ListNode listNode = new ListNode(Integer.parseInt(values[i]));
+            current.next = listNode;
+            current = listNode;
+        }
+        return result;
+    }
+
+    public BigDecimal ListNodeToBigDecimal(ListNode head) {
+        StringBuilder resultBuilder = new StringBuilder();
+        ListNode current = head;
+        while(current != null) {
+            resultBuilder.append(current.val);
+            current = current.next;
+        }
+        return new BigDecimal(resultBuilder.reverse().toString());
+    }
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        BigDecimal val1 = ListNodeToBigDecimal(l1);
+        BigDecimal val2 = ListNodeToBigDecimal(l2);
+        return BigDecimalToListNode(val1.add(val2));
     }
 }
--- java-basic/src/test/java/brute/AddTwoNumbersTest.java
+++ java-basic/src/test/java/brute/AddTwoNumbersTest.java
@@ -3,12 +3,29 @@ package brute;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+
 public class AddTwoNumbersTest {
     AddTwoNumbers solver;
 
     @Before
     public void init() {
         solver = new AddTwoNumbers();
+    }
+
+    @Test
+    public void convertTest1() {
+        ListNode result = solver.BigDecimalToListNode(new BigDecimal(123));
+        ListNodeHelper.assertListNode(new int[] {3, 2, 1}, result);
+
+    }
+
+    @Test
+    public void convertTest2() {
+        BigDecimal result = solver.ListNodeToBigDecimal(ListNodeHelper.make(new int[] {3, 2, 1}));
+        assertEquals(new BigDecimal(123), result);
     }
 
     @Test
@@ -27,5 +44,17 @@ public class AddTwoNumbersTest {
     public void simple3() {
         ListNode actual = solver.addTwoNumbers(ListNodeHelper.make(new int[] {9,9,9,9,9,9,9}), ListNodeHelper.make(new int[] {9,9,9,9}));
         ListNodeHelper.assertListNode(new int[] {8,9,9,9,0,0,0,1}, actual);
+    }
+
+    @Test
+    public void simple4() {
+        ListNode actual = solver.addTwoNumbers(ListNodeHelper.make(new int[] {9}), ListNodeHelper.make(new int[] {1,9,9,9,9,9,9,9,9,9}));
+        ListNodeHelper.assertListNode(new int[] {0,0,0,0,0,0,0,0,0,0,1}, actual);
+    }
+
+    @Test
+    public void simple5() {
+        ListNode actual = solver.addTwoNumbers(ListNodeHelper.make(new int[] {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1}), ListNodeHelper.make(new int[] {5,6,4}));
+        ListNodeHelper.assertListNode(new int[] {6,6,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1}, actual);
     }
 }
