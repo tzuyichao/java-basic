f324c86fece829f6b5c0b8171e613dee564dbc27
tzuyichao <tzuyichao@mail2000.com.tw>
2021-02-05 17:29:24 +0800

    殼

--- java-basic/src/main/java/brute/RemoveNthNodeFromEndOfList.java
+++ java-basic/src/main/java/brute/RemoveNthNodeFromEndOfList.java
@@ -1,7 +1,22 @@
 package brute;
 
 public class RemoveNthNodeFromEndOfList {
+    public int length(ListNode listNode) {
+        int count = 0;
+        ListNode current = listNode;
+        while(current != null) {
+            count += 1;
+            current = current.next;
+        }
+        return count;
+    }
+
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        int length = length(head);
+        if(length == n) {
+            return null;
+        }
+
         return null;
     }
 }
