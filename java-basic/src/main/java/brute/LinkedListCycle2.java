93990ecd264cdb918e40b831128e902aa2bc538b
tzuyichao <tzuyichao@mail2000.com.tw>
2021-02-08 16:41:30 +0800

    暴力解

--- java-basic/src/main/java/brute/LinkedListCycle2.java
+++ java-basic/src/main/java/brute/LinkedListCycle2.java
@@ -1,25 +1,28 @@
 package brute;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * = 142
  * v1:
  * Runtime: 3 ms, faster than 24.15% of Java online submissions for Linked List Cycle II.
  * Memory Usage: 40.4 MB, less than 21.11% of Java online submissions for Linked List Cycle II.
+ * v2:
+ * Runtime: 4 ms, faster than 15.54% of Java online submissions for Linked List Cycle II.
+ * Memory Usage: 39.9 MB, less than 31.56% of Java online submissions for Linked List Cycle II.
  */
 public class LinkedListCycle2 {
     public ListNode detectCycle(ListNode head) {
         if(null == head || null == head.next) {
             return null;
         }
-        Map<ListNode, Boolean> store = new HashMap();
+        Set<ListNode> visitedStore = new HashSet<>();
         while(head != null) {
-            if(store.containsKey(head)) {
+            if(visitedStore.contains(head)) {
                 return head;
             } else {
-                store.put(head, Boolean.TRUE);
+                visitedStore.add(head);
             }
             head = head.next;
         }
