3bf05d0bf0bf56726dbcb854b560a787bd3fbbdb
tzuyichao <tzuyichao@mail2000.com.tw>
2021-02-09 16:12:24 +0800

    暴力

--- java-basic/src/main/java/brute/ConvertSortedList2BinarySearchTree.java
+++ java-basic/src/main/java/brute/ConvertSortedList2BinarySearchTree.java
@@ -5,14 +5,21 @@ import java.util.List;
 
 /**
  * = 109
+ * v1:
+ * Runtime: 1 ms, faster than 44.27% of Java online submissions for Convert Sorted List to Binary Search Tree.
+ * Memory Usage: 40.1 MB, less than 45.11% of Java online submissions for Convert Sorted List to Binary Search Tree.
  */
 public class ConvertSortedList2BinarySearchTree {
-    public int treeDepth(TreeNode tree) {
-        return 0;
-    }
-
-    public TreeNode makeTree(List<Integer> data, int rootIndex) {
-        return null;
+    public TreeNode createTreeNode(List<Integer> nums, int start, int end) {
+        if(start > end) {
+            return null;
+        } else {
+            int mid = (start + end)/2;
+            TreeNode node = new TreeNode(nums.get(mid));
+            node.left = createTreeNode(nums, start, mid-1);
+            node.right = createTreeNode(nums, mid+1, end);
+            return node;
+        }
     }
 
     public TreeNode sortedListToBST(ListNode head) {
@@ -29,6 +36,6 @@ public class ConvertSortedList2BinarySearchTree {
             store.add(head.val);
             head = head.next;
         }
-        return null;
+        return createTreeNode(store, 0, store.size()-1);
     }
 }
