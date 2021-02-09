e63d6b1e06b7b77f6251c988f4ea083581a9f394
tzuyichao <tzuyichao@mail2000.com.tw>
2021-02-09 17:22:58 +0800

    殼

---
+++ java-basic/src/main/java/brute/BinaryTreeRightSideView.java
@@ -0,0 +1,15 @@
+package brute;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * = 199
+ */
+public class BinaryTreeRightSideView {
+    // Custom Testcases:
+    // [1,2,4,null,5,3,null]
+    public List<Integer> rightSideView(TreeNode root) {
+        return Collections.EMPTY_LIST;
+    }
+}
