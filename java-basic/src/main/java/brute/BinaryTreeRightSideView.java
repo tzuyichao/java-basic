2c204ceb4d58ece23ae39a688cdfa41b0e4f5da5
tzuyichao <tzuyichao@mail2000.com.tw>
2021-02-09 17:27:07 +0800

    殼

--- java-basic/src/main/java/brute/BinaryTreeRightSideView.java
+++ java-basic/src/main/java/brute/BinaryTreeRightSideView.java
@@ -1,7 +1,10 @@
 package brute;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
 
 /**
  * = 199
@@ -9,10 +12,13 @@ import java.util.List;
 public class BinaryTreeRightSideView {
     // Custom Testcases:
     // [1,2,4,null,5,3,null]
+    // []
     public List<Integer> rightSideView(TreeNode root) {
         if(null == root) {
             return Collections.EMPTY_LIST;
         }
-        return Collections.EMPTY_LIST;
+        Queue<Integer> result = new LinkedList<>();
+
+        return result.stream().collect(Collectors.toList());
     }
 }
