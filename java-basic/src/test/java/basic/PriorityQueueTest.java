2065b83e4150d6888da520457a7262609b8b1bfe
tzuyichao <tzuyichao@mail2000.com.tw>
2021-02-17 08:58:27 +0800

    測試

--- java-basic/src/test/java/basic/PriorityQueueTest.java
+++ java-basic/src/test/java/basic/PriorityQueueTest.java
@@ -26,16 +26,40 @@ public class PriorityQueueTest {
     @Test
     public void simple2() {
         Comparator<List<Integer>> c = (o1, o2) -> {
-            return o1.stream().reduce(0, (a, b) -> a+b).compareTo(o2.stream().reduce(0, (a, b) -> a+b));
+            int io1 = o1.stream().reduce(0, (a, b) -> a+b).intValue();
+            int io2 = o2.stream().reduce(0, (a, b) -> a+b).intValue();
+            System.out.println(io1 + ":" + io2);
+            return Integer.compare(io1, io2);
         };
-        PriorityQueue priorityQueue = new PriorityQueue(3, c);
+        PriorityQueue<List<Integer>> priorityQueue = new PriorityQueue(3, c);
         System.out.println(priorityQueue);
-        priorityQueue.add(List.of(1, 2));
-        priorityQueue.add(List.of(2, 2));
+        priorityQueue.add(List.of(1, 3));
+        priorityQueue.add(List.of(1, 5));
         System.out.println(priorityQueue);
-        priorityQueue.add(List.of(0, 1));
+        priorityQueue.add(List.of(1, 7));
         System.out.println(priorityQueue);
-        priorityQueue.add(List.of(0, 0));
+        priorityQueue.add(List.of(1, 9));
+        System.out.println(priorityQueue);
+        priorityQueue.add(List.of(2, 3));
+        System.out.println(priorityQueue);;
+        priorityQueue.add(List.of(2, 5));
+        System.out.println(priorityQueue);;
+    }
+
+    @Test
+    public void simple3() {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(4);
+        System.out.println(priorityQueue);
+        priorityQueue.add(6);
+        System.out.println(priorityQueue);
+        priorityQueue.add(8);
+        System.out.println(priorityQueue);
+        priorityQueue.add(10);
+        System.out.println(priorityQueue);
+        priorityQueue.add(5);
+        System.out.println(priorityQueue);
+        priorityQueue.add(7);
         System.out.println(priorityQueue);
     }
 }
