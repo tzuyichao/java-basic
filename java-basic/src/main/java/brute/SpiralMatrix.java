89bd86880f03d0374571fe1b972fb8dbd7ccc241
tzuyichao <tzuyichao@mail2000.com.tw>
2021-02-17 17:23:57 +0800

    殼

---
+++ java-basic/src/main/java/brute/SpiralMatrix.java
@@ -0,0 +1,12 @@
+package brute;
+
+import java.util.List;
+
+/**
+ * = 54
+ */
+public class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        return null;
+    }
+}
---
+++ java-basic/src/test/java/brute/SpiralMatrixTest.java
@@ -0,0 +1,30 @@
+package brute;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SpiralMatrixTest {
+    SpiralMatrix solver;
+
+    @Before
+    public void init() {
+        solver = new SpiralMatrix();
+    }
+
+    @Test
+    public void simple1() {
+        assertEquals(new int[] {1,2,3,6,9,8,7,4,5}, solver.spiralOrder(new int[][] {{1,2,3},{4,5,6},{7,8,9}}));
+    }
+
+    @Test
+    public void simple2() {
+        assertEquals(new int[] {1,2,3,4,8,12,11,10,9,5,6,7}, solver.spiralOrder(new int[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12}}));
+    }
+
+    @Test
+    public void simple3() {
+        assertEquals(new int[] {1,2,3,4}, solver.spiralOrder(new int[][] {{1,2,3,4}}));
+    }
+}
