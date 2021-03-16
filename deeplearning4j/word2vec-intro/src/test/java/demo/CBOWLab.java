9197998a030ca9202f89cc99c7d7e6cb90c80d53
tzuyichao <tzuyichao@mail2000.com.tw>
2021-03-16 15:23:56 +0800

    結果不太依樣

--- deeplearning4j/word2vec-intro/src/test/java/demo/CBOWLab.java
+++ deeplearning4j/word2vec-intro/src/test/java/demo/CBOWLab.java
@@ -5,6 +5,9 @@ import org.deeplearning4j.models.embeddings.learning.impl.elements.CBOW;
 import org.deeplearning4j.models.word2vec.Word2Vec;
 import org.deeplearning4j.text.sentenceiterator.BasicLineIterator;
 import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
+import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
 import org.junit.Test;
 import org.nd4j.linalg.io.ClassPathResource;
 
@@ -29,6 +32,30 @@ public class CBOWLab {
         vec.fit();
 
         String[] words = new String[] {"guitar", "love", "rock"};
+        for(String w: words) {
+            Collection<String> lst = vec.wordsNearest(w, 2);
+            System.out.println("2 words closest to " + w + ": " + lst);
+        }
+    }
+
+    @Test
+    public void simple2() throws IOException {
+        String filePath = new ClassPathResource("raw_sentences.txt").getFile().getAbsolutePath();
+        SentenceIterator iterator = new BasicLineIterator(filePath);
+
+        TokenizerFactory tokenizerFactory = new DefaultTokenizerFactory();
+        tokenizerFactory.setTokenPreProcessor(new CommonPreprocessor());
+
+        Word2Vec vec = new Word2Vec.Builder()
+                .layerSize(100)
+                .windowSize(5)
+                .iterate(iterator)
+                .tokenizerFactory(tokenizerFactory)
+                .elementsLearningAlgorithm(new CBOW<>())
+                .build();
+        vec.fit();
+
+        String[] words = new String[] {"day", "play", "music", "still"};
         for(String w: words) {
             Collection<String> lst = vec.wordsNearest(w, 2);
             System.out.println("2 words closest to " + w + ": " + lst);
