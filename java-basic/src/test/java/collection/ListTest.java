830d37f2fcf286ed342d96d63da9bc99ca719a48
tzuyichao <tzuyichao@mail2000.com.tw>
2021-03-18 10:07:31 +0800

    測試

--- java-basic/src/test/java/collection/ListTest.java
+++ java-basic/src/test/java/collection/ListTest.java
@@ -2,10 +2,10 @@ package collection;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ListTest {
     @Test
@@ -13,5 +13,20 @@ public class ListTest {
         List<Integer> l = new ArrayList<>();
         l.add(null);
         assertEquals(1, l.size());
+    }
+
+    @Test
+    public void test_equals_of_list_of() {
+        assertEquals(List.of(2, 2), List.of(2, 2));
+        assertTrue(List.of(2, 2).equals(List.of(2, 2)));
+    }
+
+    @Test
+    public void test_set() {
+        Set<List<Integer>> set = new HashSet<>();
+        set.add(List.of(2, 2));
+        set.add(Collections.EMPTY_LIST);
+        set.add(List.of(2, 2));
+        assertEquals(2, set.size());
     }
 }
