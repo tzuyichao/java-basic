b8642b644cb3c367d0f10d11858b6335898864d3
tzuyichao <tzuyichao@mail2000.com.tw>
2021-04-07 09:29:48 +0800

    測試 Math.round()

---
+++ java-basic/src/main/java/math/RoundLab.java
@@ -0,0 +1,24 @@
+package math;
+
+public class RoundLab {
+    /**
+     * PS C:\Users\terence.chao> python
+     * Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+     * Type "help", "copyright", "credits" or "license" for more information.
+     * >>> round(1.5)
+     * 2
+     * >>> round(2.5)
+     * 2
+     * >>> round(3.5)
+     * 4
+     * >>> round(4.5)
+     * 4
+     *
+     */
+    public static void main(String[] args) {
+        System.out.println("round(1.5)=" + Math.round(1.5));
+        System.out.println("round(2.5)=" + Math.round(2.5));
+        System.out.println("round(3.5)=" + Math.round(3.5));
+        System.out.println("round(4.5)=" + Math.round(4.5));
+    }
+}
