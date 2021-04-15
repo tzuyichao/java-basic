8f28e09665058e32e937c833069b156ef5cc4a48
tzuyichao <tzuyichao@mail2000.com.tw>
2021-04-15 17:10:55 +0800

    TemporalAdjuster example

--- java-basic/src/main/java/time/TemporalAdjusterExample.java
+++ java-basic/src/main/java/time/TemporalAdjusterExample.java
@@ -2,6 +2,9 @@ package time;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoField;
+import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
@@ -15,6 +18,13 @@ public class TemporalAdjusterExample {
         LocalDate localDate = LocalDate.parse("2020-05-09");
         List<LocalDate> dates = getPeriodicMeetingDates(localDate, TemporalAdjusters.next(DayOfWeek.MONDAY), 5);
         System.out.println(dates);
+
+        LocalDate toBirth = localDate.with(new MoveToBirthDay());
+        System.out.println(toBirth);
+
+        TemporalAdjuster nextSevenDay = t -> t.plus(Period.ofDays(7));
+        LocalDate sevenDaysLater = localDate.with(nextSevenDay);
+        System.out.println(sevenDaysLater);
     }
 
     private static List<LocalDate> getPeriodicMeetingDates(final LocalDate startDate, final TemporalAdjuster adjuster, final int numbers) {
@@ -26,5 +36,13 @@ public class TemporalAdjusterExample {
         }
 
         return res;
+    }
+
+    static class MoveToBirthDay implements TemporalAdjuster {
+        @Override
+        public Temporal adjustInto(Temporal temporal) {
+            return temporal.with(ChronoField.MONTH_OF_YEAR, 1)
+                    .with(ChronoField.DAY_OF_MONTH, 11);
+        }
     }
 }
