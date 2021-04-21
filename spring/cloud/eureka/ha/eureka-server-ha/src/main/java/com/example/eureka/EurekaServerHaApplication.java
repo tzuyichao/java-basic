88382c2dd95fc9cdb72323d8836de1586fc050dd
tzuyichao <tzuyichao@mail2000.com.tw>
2021-04-21 09:18:56 +0800

    first add

---
+++ spring/cloud/eureka/ha/eureka-server-ha/src/main/java/com/example/eureka/EurekaServerHaApplication.java
@@ -0,0 +1,13 @@
+package com.example.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerHaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerHaApplication.class, args);
+	}
+
+}
---
+++ spring/cloud/eureka/ha/eureka-server-ha/src/test/java/com/example/eureka/EurekaServerHaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerHaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
