dccd95ed109dcd671c052a2807f82905837c3558
TzuYi Chao <tzuyichao0313@gmail.com>
2021-11-01 15:35:42 +0800

    partial spring-boot + jooq from Baeldung

--- spring/persistence/spring-jooq/src/test/java/app/CountQueryIntegrationTest.java
+++ spring/persistence/spring-jooq/src/test/java/app/CountQueryIntegrationTest.java
@@ -3,7 +3,11 @@ package app;
 import static com.baeldung.jooq.introduction.db.public_.tables.Author.AUTHOR;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import org.jooq.Condition;
 import org.jooq.DSLContext;
+import org.jooq.Record2;
+import org.jooq.Result;
+import org.jooq.impl.DSL;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,5 +21,24 @@ public class CountQueryIntegrationTest {
     public void givenValidData_whenSimpleSelect_thenSuccess() {
         int count = dsl.select().from(AUTHOR).execute();
         assertThat(count).isEqualTo(3);
+    }
+
+    @Test
+    public void givenValidData_whenCountWithGroupBy_thenSuccess() {
+        final Result<Record2<String, Integer>> result = dsl.select(AUTHOR.FIRST_NAME, DSL.count())
+                .from(AUTHOR)
+                .groupBy(AUTHOR.FIRST_NAME).fetch();
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result.get(0).get(0)).isEqualTo("Bert");
+        assertThat(result.get(0).get(1)).isEqualTo(1);
+    }
+
+    @Test
+    public void givenValidData_whenFetchCountWithConditionInVarargs_thenSuccess() {
+        Condition fistCond = AUTHOR.FIRST_NAME.endsWithIgnoreCase("Bryan");
+        Condition secondCond = AUTHOR.ID.notEqual(1);
+
+        int count = dsl.fetchCount(AUTHOR, fistCond, secondCond);
+        assertThat(count).isEqualTo(1);
     }
 }
