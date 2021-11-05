72ccc147ef5b682dcd8702b5044fca197673b382
TzuYi Chao <tzuyichao0313@gmail.com>
2021-11-05 12:42:29 +0800

    r2dbc h2 test - simple connection

---
+++ r2dbc/connection/src/main/java/main/SimpleConnectionApp.java
@@ -0,0 +1,22 @@
+package main;
+
+import io.r2dbc.h2.H2Connection;
+import io.r2dbc.h2.H2ConnectionConfiguration;
+import io.r2dbc.h2.H2ConnectionFactory;
+import io.r2dbc.h2.H2ConnectionOption;
+import reactor.core.publisher.Mono;
+
+public class SimpleConnectionApp {
+    public static void main(String[] args) {
+        H2ConnectionFactory connectionFactory = new H2ConnectionFactory(H2ConnectionConfiguration.builder()
+                .inMemory("testdb")
+                .property(H2ConnectionOption.DB_CLOSE_DELAY, "-1")
+                .build());
+        Mono<H2Connection> connection = connectionFactory.create();
+        System.out.println(connection);
+        connection.subscribe(conn -> {
+            System.out.println(conn);
+            System.out.println(Thread.currentThread().getName());
+        });
+    }
+}
