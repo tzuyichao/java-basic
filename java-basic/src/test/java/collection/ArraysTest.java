f750254d7dd2d5343724aa65b8d53cd6e1f60002
TzuYi Chao <tzuyichao0313@gmail.com>
2021-11-17 14:14:02 +0800

    Arrays test

---
+++ java-basic/src/test/java/collection/ArraysTest.java
@@ -0,0 +1,21 @@
+package collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArraysTest {
+    @Test
+    public void test_first_mismatch_index() {
+        int[] integers1 = {3, 4, 5, 6, 1, 5, 3, 4};
+        int[] integers2 = {3, 4, 5, 6, 1, 5, 3, 4};
+        int[] integers3 = {3, 4, 5, 6, 1, 3, 4, 4};
+
+        assertThat(Arrays.mismatch(integers1, integers2))
+                .isEqualTo(-1);
+        assertThat(Arrays.mismatch(integers1, integers3))
+                .isEqualTo(5);
+    }
+}
