b7dcede4bceb294e04d052f0757349aedd523f90
TzuYi Chao <tzuyichao0313@gmail.com>
2021-11-17 15:03:48 +0800

    Arrays test

--- java-basic/src/test/java/collection/ArraysTest.java
+++ java-basic/src/test/java/collection/ArraysTest.java
@@ -18,4 +18,16 @@ public class ArraysTest {
         assertThat(Arrays.mismatch(integers1, integers3))
                 .isEqualTo(5);
     }
+
+    @Test
+    public void test_arrays_lexicographically() {
+        int[] integers1 = {3, 4, 5, 6, 1, 5, 3, 4};
+        int[] integers2 = {3, 4, 5, 6, 1, 5, 3, 4};
+        int[] integers3 = {3, 4, 5, 6, 1, 3, 4, 4};
+
+        assertThat(Arrays.compare(integers1, integers2))
+                .isEqualTo(0);
+        assertThat(Arrays.compare(integers1, integers3))
+                .isEqualTo(1);
+    }
 }
