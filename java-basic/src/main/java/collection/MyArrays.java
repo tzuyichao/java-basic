d6e44e828dce7f52afe42837712a22ebc75b0f02
TzuYi Chao <tzuyichao0313@gmail.com>
2021-11-17 13:20:54 +0800

    contains element

--- java-basic/src/main/java/collection/MyArrays.java
+++ java-basic/src/main/java/collection/MyArrays.java
@@ -1,9 +1,26 @@
 package collection;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Objects;
 import java.util.stream.IntStream;
 
 public class MyArrays {
+    public static boolean containsElement(int[] arr, int toContain) {
+        Objects.requireNonNull(arr, "arr can't be null");
+        return Arrays.stream(arr)
+                .anyMatch(e -> e == toContain);
+    }
+
+    public static <T> boolean containsElement(T[] arr, T toContain, Comparator<? super T> comparator) {
+        for(T elem: arr) {
+            if(comparator.compare(elem, toContain) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static int findIndexOfElement(int[] arr, int toFind) {
         Objects.requireNonNull(arr, "arr can't be null");
         return IntStream.range(0, arr.length)
--- java-basic/src/test/java/collection/MyArraysTest.java
+++ java-basic/src/test/java/collection/MyArraysTest.java
@@ -1,10 +1,44 @@
 package collection;
 
+import collection.sort.Melon;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MyArraysTest {
+    @Test
+    public void test_containsElement_happy() {
+        int[] arr = new int[]{4, 5, 7, 1, 2};
+        assertThat(MyArrays.containsElement(arr, 7))
+                .isTrue();
+    }
+
+    @Test
+    public void test_containsElement_not_found() {
+        int[] arr = new int[]{4, 5, 7, 1, 2};
+        assertThat(MyArrays.containsElement(arr, 8))
+                .isFalse();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void test_containsElement_null_array() {
+        MyArrays.containsElement(null, 0);
+    }
+
+    @Test
+    public void test_generic_containsElement_happy() {
+        Melon[] melons = new Melon[] {
+                new Melon("cantaloupe", 5),
+                new Melon("watermelon", 11),
+                new Melon("hami", 2),
+                new Melon("snap melon", 6)
+        };
+        assertThat(MyArrays.containsElement(melons, new Melon("watermelon", 13), (m1, m2) -> {
+            return m1.getType().compareTo(m2.getType());
+        }))
+                .isTrue();
+    }
+
     @Test
     public void test_findIndexOfElement_happy() {
         int[] arr = new int[]{4, 5, 7, 1, 2};
