a8be27f0f01ad26424cb6a5e141b9db1b85bf11d
TzuYi Chao <tzuyichao0313@gmail.com>
2021-11-18 11:01:45 +0800

    * LeetCode: 32 Longest Valid Parentheses  * Time Limit Exceeded

--- java-basic/src/test/java/brute/LongestValidParenthesesTest.java
+++ java-basic/src/test/java/brute/LongestValidParenthesesTest.java
@@ -43,6 +43,7 @@ public class LongestValidParenthesesTest {
                 .isEqualTo(6);
     }
 
+    // PT0.2198947S
     @Test
     public void testHappy6() {
         LongestValidParentheses solution = new LongestValidParentheses();
