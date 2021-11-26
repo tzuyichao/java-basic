244a402303f901797b266004f0b1f3a67ebfd5ba
TzuYi Chao <tzuyichao0313@gmail.com>
2021-11-26 16:50:14 +0800

    multiple datasources

--- quarkus/persistent/src/main/java/org/acme/persistent/GreetingResource.java
+++ quarkus/persistent/src/main/java/org/acme/persistent/GreetingResource.java
@@ -1,5 +1,8 @@
 package org.acme.persistent;
 
+import io.agroal.api.AgroalDataSource;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,6 +10,16 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
+    @Inject
+    AgroalDataSource dataSource;
+
+    @Inject
+    @io.quarkus.agroal.DataSource("users")
+    AgroalDataSource userDataSource;
+
+    @Inject
+    @io.quarkus.agroal.DataSource("inventory")
+    AgroalDataSource inventoryDataSource;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
