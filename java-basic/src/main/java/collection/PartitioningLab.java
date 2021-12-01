31bba8d34ed8793a02de9709cebadd246edc4d13
TzuYi Chao <tzuyichao0313@gmail.com>
2021-12-01 14:07:02 +0800

    partitioningBy lab

--- java-basic/src/main/java/collection/PartitioningLab.java
+++ java-basic/src/main/java/collection/PartitioningLab.java
@@ -3,6 +3,7 @@ package collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.partitioningBy;
@@ -10,6 +11,18 @@ import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.maxBy;
 
 public class PartitioningLab {
+    public static boolean isPrime(int candidate) {
+        int candidateRoot = (int) Math.sqrt((double) candidate);
+        return IntStream.rangeClosed(2, candidateRoot)
+                .noneMatch(i -> candidate % i == 0);
+    }
+
+    public static Map<Boolean, List<Integer>> partitionPrimes(int n) {
+        return IntStream.rangeClosed(2, n)
+                .boxed()
+                .collect(partitioningBy(candidate -> isPrime(candidate)));
+    }
+
     public static void main(String[] args) {
         Map<Boolean, List<Dish>> partitionedMenu = Dishes.getMenu().stream()
                 .collect(partitioningBy(Dish::isVegetarian));
@@ -19,5 +32,8 @@ public class PartitioningLab {
                 .collect(partitioningBy(Dish::isVegetarian,
                         collectingAndThen(maxBy(comparingInt(Dish::getCalories)), Optional::get)));
         System.out.println(mostCaloricPartitioningByVegetarian);
+
+        Map<Boolean, List<Integer>> primes = partitionPrimes(100);
+        System.out.println(primes);
     }
 }
