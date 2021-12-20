649e506c0e8fceeaa180dcc226a84a34fbbddca6
TzuYi Chao <tzuyichao0313@gmail.com>
2021-12-20 11:32:30 +0800

    doOnxxxx partial

--- spring/rsb/reactor/src/test/java/rsb/reactor/DoOnTest.java
+++ spring/rsb/reactor/src/test/java/rsb/reactor/DoOnTest.java
@@ -2,7 +2,11 @@ package rsb.reactor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Signal;
+import reactor.core.publisher.SignalType;
+import reactor.test.StepVerifier;
 
 import java.util.ArrayList;
 
@@ -10,12 +14,30 @@ import java.util.ArrayList;
 public class DoOnTest {
     @Test
     public void doOn() {
-        var signals = new ArrayList<Integer>();
+        var signals = new ArrayList<Signal>();
         var nextValues = new ArrayList<Integer>();
+        var subscriptions = new ArrayList<Subscription>();
+        var exceptions = new ArrayList<Throwable>();
+        var finallySignals = new ArrayList<SignalType>();
 
         Flux<Integer> on = Flux.<Integer>create(integerFluxSink -> {
+                integerFluxSink.next(1);
+                integerFluxSink.next(2);
+                integerFluxSink.next(3);
+                integerFluxSink.error(new IllegalArgumentException("oops"));
+                integerFluxSink.complete();
+            })
+                .doOnNext(nextValues::add)
+                .doOnEach(signals::add)
+                .doOnSubscribe(subscriptions::add)
+                .doOnError(IllegalArgumentException.class, exceptions::add)
+                .doFinally(finallySignals::add);
 
-        })
-                .doOnNext(nextValues::add);
+        StepVerifier.create(on)
+                .expectNext(1, 2, 3)
+                .expectError(IllegalArgumentException.class)
+                .verify();
+
+        signals.forEach(s -> log.info("Signal: {}", s));
     }
 }
