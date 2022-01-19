76a5acafcc3ee00c7fc10e4f09a42295a49632dd
TzuYi Chao <tzuyichao0313@gmail.com>
2022-01-19 17:12:18 +0800

    rename

--- spring/webflux/webflux-demo/src/main/java/org/greenrivers/spring/webfluxdemo/WebFluxDemoApplication.java
+++ spring/webflux/webflux-demo/src/main/java/org/greenrivers/spring/webfluxdemo/WebFluxDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebfluxDemoApplication {
+public class WebFluxDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebfluxDemoApplication.class, args);
+		SpringApplication.run(WebFluxDemoApplication.class, args);
 	}
 
 }
