8c67f66e5658031b82611ba75a0020e274c79d98
TzuYi Chao <tzuyichao0313@gmail.com>
2022-01-20 14:25:11 +0800

    zip test

---
+++ spring/rsb/reactor/src/test/java/rsb/reactor/ControlFlowZipTest.java
@@ -0,0 +1,44 @@
+package rsb.reactor;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class ControlFlowZipTest {
+    @Test
+    public void zip() {
+        var first = Flux.just(1, 2, 3);
+        var second = Flux.just("a", "b", "c");
+        var zip = Flux.zip(first, second)
+                .map(tuple -> this.from(tuple.getT1(), tuple.getT2()));
+        StepVerifier.create(zip)
+                .expectNext("1:a", "2:b", "3:c")
+                .verifyComplete();
+    }
+
+    @Test
+    public void zip2() {
+        var first = Flux.just(1, 2, 3, 4);
+        var second = Flux.just("a", "b", "c");
+        var zip = Flux.zip(first, second)
+                .map(tuple -> this.from(tuple.getT1(), tuple.getT2()));
+        StepVerifier.create(zip)
+                .expectNext("1:a", "2:b", "3:c")
+                .verifyComplete();
+    }
+
+    @Test
+    public void zip3() {
+        var first = Flux.just(1, 2, 3);
+        var second = Flux.just("a", "b", "c", "d");
+        var zip = Flux.zip(first, second)
+                .map(tuple -> this.from(tuple.getT1(), tuple.getT2()));
+        StepVerifier.create(zip)
+                .expectNext("1:a", "2:b", "3:c")
+                .verifyComplete();
+    }
+
+    private String from(Integer i, String s) {
+        return i + ":" + s;
+    }
+}
