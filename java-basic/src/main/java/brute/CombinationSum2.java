2f966891e86b4df4019152cd41856831ad960561
TzuYi Chao <tzuyichao0313@gmail.com>
2022-02-14 13:15:48 +0800

    completed

--- java-basic/src/main/java/brute/CombinationSum2.java
+++ java-basic/src/main/java/brute/CombinationSum2.java
@@ -4,47 +4,44 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 40. Combination Sum II
  * [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
  * 27
  * Time Limit Exceeded
+ *
+ * Runtime: 6 ms, faster than 50.67% of Java online submissions for Combination Sum II.
+ * Memory Usage: 44.5 MB, less than 8.49% of Java online submissions for Combination Sum II.
  */
 public class CombinationSum2 {
-    private void findResult(int[] candidates, int target, int currIdx, int rightIdx, List<Integer> current, List<List<Integer>> collector) {
-        int sum = current.stream().reduce(0, Integer::sum);
-        if(target == sum) {
-            if(!collector.contains(current)) {
-                collector.add(current);
+    private void findResult(int[] candidates, int target, int currIdx, List<Integer> current, List<List<Integer>> collector) {
+        if(target < 0) {
+            return;
+        }
+        if(target == 0) {
+            collector.add(current);
+        }
+        for(int i=currIdx; i<candidates.length; i++) {
+            if(i > currIdx && candidates[i] == candidates[i-1]) {
+                continue;
             }
-        } else if (target > sum) {
-            for(int i=currIdx+1; i<rightIdx; i++) {
+            if(target - candidates[i] >= 0) {
                 List<Integer> c = new ArrayList<>(current);
                 c.add(candidates[i]);
-                findResult(candidates, target, i, rightIdx, c, collector);
+                findResult(candidates, target-candidates[i], i+1, c, collector);
             }
         }
-        return;
     }
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        int l = candidates.length;
-        int r = candidates.length;
-        for(int i=l-1; i>0; i--) {
-            if(candidates[i] <= target) {
-                r = i+1;
-                break;
-            }
-        }
         List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
 
-        for(int i=0; i<r; i++) {
-            List<Integer> current = new ArrayList<>();
-            current.add(candidates[i]);
-            findResult(candidates, target, i, r, current, result);
-        }
+        findResult(candidates, target, 0, current, result);
 
         return result;
     }
