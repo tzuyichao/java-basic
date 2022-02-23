6ce26d3fc0878a844c900e2d845bc299bcb9eddd
TzuYi Chao <tzuyichao0313@gmail.com>
2022-02-23 10:03:52 +0800

    add memo

--- java-basic-2/src/main/java/brute/InsertInterval.java
+++ java-basic-2/src/main/java/brute/InsertInterval.java
@@ -16,16 +16,19 @@ public class InsertInterval {
         var n = intervals.length;
         var c = 0;
         var res = new ArrayList<int[]>();
+        // first part: before newInterval and no overlap
         while(c < n && intervals[c][1] < newInterval[0]) {
             res.add(intervals[c]);
             c++;
         }
+        // second part: has overlap with newInterval, change newInterval index 0 and 1 fit requirement
         while(c < n && intervals[c][0] <= newInterval[1]) {
             newInterval[0] = Math.min(newInterval[0], intervals[c][0]);
             newInterval[1] = Math.max(newInterval[1], intervals[c][1]);
             c++;
         }
         res.add(newInterval);
+        // third part: after newInterval and no overlap
         while(c < n) {
             res.add(intervals[c]);
             c++;
