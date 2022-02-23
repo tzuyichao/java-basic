27eab267a3f49b214975928ea8d5623cdf8d57a8
TzuYi Chao <tzuyichao0313@gmail.com>
2022-02-23 08:01:11 +0800

    add stone game test

---
+++ java-basic-2/src/test/java/brute/StoneGameTest.java
@@ -0,0 +1,14 @@
+package brute;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class StoneGameTest {
+    @Test
+    public void happy() {
+        var solver = new StoneGame();
+        assertThat(solver.stoneGame(new int[] {7,7,12,16,41,48,41,48,11,9,34,2,44,30,27,12,11,39,31,8,23,11,47,25,15,23,4,17,11,50,16,50,38,34,48,27,16,24,22,48,50,10,26,27,9,43,13,42,46,24}), is(true));
+    }
+}
