4df6e0814046e2be73ec58c5b7b83ff6eb1f61f1
TzuYi Chao <tzuyichao0313@gmail.com>
2022-03-09 10:18:40 +0800

    add ref

--- java-basic-2/src/main/java/brute/LargestNumber.java
+++ java-basic-2/src/main/java/brute/LargestNumber.java
@@ -9,6 +9,8 @@ import java.util.stream.Collectors;
  *
  * Runtime: 24 ms, faster than 8.01% of Java online submissions for Largest Number.
  * Memory Usage: 44.6 MB, less than 17.08% of Java online submissions for Largest Number.
+ *
+ * https://medium.com/@ChYuan/leetcode-no-179-largest-number-%E5%BF%83%E5%BE%97-medium-e578b1d9b89e
  */
 public class LargestNumber {
 
