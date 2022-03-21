8628581f03ba6fc0634f5d226710ea0005cd3100
TzuYi Chao <tzuyichao0313@gmail.com>
2022-03-21 15:11:51 +0800

    test

---
+++ java-basic-2/src/main/java/lang/CharacterIsIdeographicTest.java
@@ -0,0 +1,12 @@
+public class CharacterIsIdeographicTest {
+  public static boolean isCJK(String str) {
+    return str.codePoints().allMatch(Character::isIdeographic);
+  }
+  
+  public static void main(String[] args) {
+    String str1 = "測試1";
+    String str2 = "測試二";
+    System.out.println(isCJK(str1));
+    System.out.println(isCJK(str2));
+  }
+}
