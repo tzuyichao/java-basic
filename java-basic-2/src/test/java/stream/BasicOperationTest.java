892b733876fea137cbabfdc29d9d71fd10f712f8
TzuYi Chao <tzuyichao0313@gmail.com>
2022-08-08 14:12:03 +0800

    unit test with contains()

---
+++ java-basic-2/src/test/java/stream/BasicOperationTest.java
@@ -0,0 +1,17 @@
+package stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class BasicOperationTest {
+    @Test
+    void test_filter() {
+        List<Integer> res = List.of(1, 2, 3, 4).stream().filter(item -> item % 2 == 0).collect(Collectors.toList());
+        assertThat(res, contains(2, 4));
+    }
+}
