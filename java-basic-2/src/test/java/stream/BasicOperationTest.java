b7cc8369907597884d898d3c61dd664d70215211
TzuYi Chao <tzuyichao0313@gmail.com>
2022-08-09 09:30:36 +0800

    more test cases

--- java-basic-2/src/test/java/stream/BasicOperationTest.java
+++ java-basic-2/src/test/java/stream/BasicOperationTest.java
@@ -14,4 +14,11 @@ public class BasicOperationTest {
         List<Integer> res = List.of(1, 2, 3, 4).stream().filter(item -> item % 2 == 0).collect(Collectors.toList());
         assertThat(res, contains(2, 4));
     }
+
+    @Test
+    void test_filter2() {
+        List<Integer> ids = List.of(1, 3);
+        List<Integer> res = List.of(1, 2, 3, 4).stream().filter(id -> !ids.stream().anyMatch(i -> i == id)).collect(Collectors.toList());
+        assertThat(res, contains(2, 4));
+    }
 }
