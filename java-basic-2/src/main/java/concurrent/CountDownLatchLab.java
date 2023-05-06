0fa1cd23aa0e33fc55034e7e560252129100a159
TzuYi Chao <tzuyichao0313@gmail.com>
2023-05-06 16:25:45 +0800

    more test

--- java-basic-2/src/main/java/concurrent/CountDownLatchLab.java
+++ java-basic-2/src/main/java/concurrent/CountDownLatchLab.java
@@ -1,6 +1,7 @@
 package concurrent;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class CountDownLatchLab {
     static class Worker implements Runnable {
@@ -12,6 +13,11 @@ public class CountDownLatchLab {
 
         @Override
         public void run() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             if(Math.random() < 0.8) {
                 countDownLatch.countDown();
             } else {
@@ -20,15 +26,47 @@ public class CountDownLatchLab {
         }
     }
 
+    /**
+     * 監看Worker的狀態
+     */
+    static class Monitor implements Runnable {
+        Thread[] pool;
+        public Monitor(Thread[] pool) {
+            this.pool = pool;
+        }
+
+        @Override
+        public void run() {
+            while(true) {
+                for (Thread thread : pool) {
+                    System.out.println(thread.getName() + " State: " + thread.getState());
+                }
+                try {
+                    Thread.sleep(20000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
     static void run() throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(10);
+        Thread[] pool = new Thread[10];
+
         for(int i=0; i<10; i++) {
             Thread thread = new Thread(new Worker(countDownLatch));
+            pool[i] = thread;
             System.out.println("Creating Thread " + thread.getName());
             thread.start();
         }
+        Thread monitor = new Thread(new Monitor(pool));
+        monitor.setDaemon(true);
+        monitor.start();
 
-        countDownLatch.await();
+        // 如果有一個thread發生意料之外的狀況沒有countDown()就停止等情況這裡就會等到天荒地老
+        // countDownLatch.await();
+        countDownLatch.await(1, TimeUnit.MINUTES);
         System.out.println("Done");
     }
 
