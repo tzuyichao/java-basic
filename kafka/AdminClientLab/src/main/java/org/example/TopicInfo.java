90adce8fd8c812858c65e84bdcfd40c52f9fe1c0
TzuYi Chao <tzuyichao0313@gmail.com>
2023-06-12 13:57:13 +0800

    Topic info draft

---
+++ kafka/AdminClientLab/src/main/java/org/example/TopicInfo.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.common.KafkaFuture;
+import org.apache.kafka.common.Node;
+import org.apache.kafka.common.TopicPartitionInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+public class TopicInfo {
+    static Logger logger = LoggerFactory.getLogger(TopicInfo.class);
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "YOUR_BOOTSTRAP_SERVERS");
+        props.put("fetch.max.bytes", 1024);
+        props.put("enable.auto.commit", true);
+        props.put("auto.commit.interval.ms", 1000);
+        props.put("security.protocol", "SASL_PLAINTEXT");
+        props.put("sasl.mechanism", "SCRAM-SHA-512");
+        props.put("auto.offset.reset","earliest");
+        String jaas = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"ACCOUNT\" password=\"PASSWORD\";";
+        props.put("sasl.jaas.config", jaas);
+        try(AdminClient adminClient = KafkaAdminClient.create(props)) {
+            ListTopicsOptions listTopicsOptions = new ListTopicsOptions();
+            listTopicsOptions.listInternal(true);
+            ListTopicsResult listTopicsResult = adminClient.listTopics(listTopicsOptions);
+            Collection<TopicListing> topicListings = listTopicsResult.listings().get();
+            for(TopicListing topicListing : topicListings) {
+                System.out.println("Topic ID: " + topicListing.topicId() + ", Topic Name: " + topicListing.name());
+                logger.debug("Topic ID: {}, Topic Name: {}", topicListing.topicId(), topicListing.name());
+            }
+            Map<String, KafkaFuture<TopicDescription>> topicDescriptionMap = adminClient.describeTopics(topicListings.stream().map(it -> it.name()).toList()).topicNameValues();
+            topicDescriptionMap.values().stream().forEach(topicDescriptionKafkaFuture -> {
+                try {
+                    TopicDescription topicDescription = topicDescriptionKafkaFuture.get();
+                    String topicName = topicDescription.name();
+                    for(TopicPartitionInfo topicPartitionInfo: topicDescription.partitions()) {
+                        System.out.println(topicName + ", Partition: " + topicPartitionInfo.partition() + ", Leader: " + topicPartitionInfo.leader().id() + ", replicas: " + topicPartitionInfo.replicas().stream().map(node -> String.valueOf(node.id())).collect(Collectors.joining(";")));
+                    }
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
