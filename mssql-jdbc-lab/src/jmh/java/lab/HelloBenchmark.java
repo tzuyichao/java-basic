5d02d80698d92eb726ff8293395de5204a3e5ba5
TzuYi Chao <tzuyichao0313@gmail.com>
2023-07-31 22:59:26 +0800

    add

--- mssql-jdbc-lab/src/jmh/java/lab/HelloBenchmark.java
+++ mssql-jdbc-lab/src/jmh/java/lab/HelloBenchmark.java
@@ -4,13 +4,33 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * From: https://www.baeldung.com/java-microbenchmark-harness
+ */
 public class HelloBenchmark {
     @Benchmark
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @BenchmarkMode(Mode.AverageTime)
     public void doNothing() {
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @BenchmarkMode(Mode.AverageTime)
+    public void objectCreation() {
+        new Object();
+    }
+
+
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @BenchmarkMode(Mode.AverageTime)
+    public void blackHole(Blackhole blackhole) {
+        blackhole.consume(new Object());
     }
 }
