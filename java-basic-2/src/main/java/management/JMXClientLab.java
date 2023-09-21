220f836a2d3a642954b87503588459e260c84259
TERENCE.CHAO <terence.chao@deltaww.com>
2023-09-21 15:36:23 +0800

    more test

---
+++ java-basic-2/src/main/java/management/JMXClientLab.java
@@ -0,0 +1,31 @@
+package management;
+
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
+public class JMXClientLab {
+    public static void main(String[] args) {
+        try {
+            JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://10.0.0.5:9999/jmxrmi");
+            JMXConnector jmxc = JMXConnectorFactory.connect(url, null);
+            MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
+
+            // Get metrics for BytesInPerSec
+            ObjectName bytesInPerSec = new ObjectName("kafka.server:type=BrokerTopicMetrics,name=BytesInPerSec");
+            double byteRate = (double) mbsc.getAttribute(bytesInPerSec, "OneMinuteRate");
+            System.out.println("BytesInPerSec OneMinuteRate: " + byteRate);
+
+            // Get metrics for MessagesInPerSec
+            ObjectName messagesInPerSec = new ObjectName("kafka.server:type=BrokerTopicMetrics,name=MessagesInPerSec");
+            double messageRate = (double) mbsc.getAttribute(messagesInPerSec, "OneMinuteRate");
+            System.out.println("MessagesInPerSec OneMinuteRate: " + messageRate);
+
+            jmxc.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
