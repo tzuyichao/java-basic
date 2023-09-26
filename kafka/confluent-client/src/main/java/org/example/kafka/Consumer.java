dfe40df66b1b7fdbba92f44de02552a4ac9f8b91
TERENCE.CHAO <terence.chao@deltaww.com>
2023-09-26 18:19:02 +0800

    consumer lab

---
+++ kafka/confluent-client/src/main/java/org/example/kafka/Consumer.java
@@ -0,0 +1,39 @@
+package org.example.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class Consumer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "datagovstg-kfk01.deltaww.com:9093,datagovstg-kfk02.deltaww.com:9093");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "terence-test-consumer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // "earliest" or "latest"
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put("security.protocol", "SASL_PLAINTEXT");
+        props.put("sasl.mechanism", "SCRAM-SHA-512");
+        String jaas = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"ACCOUNT\" password=\"PASSWORD\";";
+        props.put("sasl.jaas.config", jaas);
+
+
+        try(KafkaConsumer consumer = new KafkaConsumer<String, String>(props);) {
+
+            consumer.subscribe(Collections.singletonList("it.mfg.schedule.job-notify.v0"));
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                records.forEach(record -> {
+                    System.out.printf("Consumed record with key %s and value %s%n", record.key(), record.value());
+                });
+            }
+        }
+    }
+}
