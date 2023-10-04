4ca295254a63a4d738b31a4f54b007c8e6052150
TERENCE.CHAO <terence.chao@deltaww.com>
2023-10-04 11:56:53 +0800

    add rmi client to lookup rmi server stub remote and query all mbeans

---
+++ java-basic-2/src/main/java/management/JMXClientStub.java
@@ -0,0 +1,35 @@
+package management;
+
+import javax.management.ObjectInstance;
+import javax.management.ObjectName;
+import javax.management.remote.rmi.RMIConnection;
+import javax.management.remote.rmi.RMIServer;
+import java.rmi.Remote;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Set;
+
+public class JMXClientStub {
+    public static void main(String[] args) throws Exception {
+        Registry registry = LocateRegistry.getRegistry("datagovstg-kfk01.deltaww.com", 9999);
+        for(String reg : registry.list()) {
+            System.out.println(reg);
+        }
+
+        Remote remote = registry.lookup("jmxrmi");
+        System.out.println("Remote: " + remote.toString());
+
+        RMIServer rmiServer = (RMIServer) remote;
+        try(RMIConnection rmiConnection = rmiServer.newClient(null);) {
+            String connectionId = rmiConnection.getConnectionId();
+            System.out.println("Connection Id: " + connectionId);
+            Set<ObjectInstance> mbeans = rmiConnection.queryMBeans(null, null, null);
+            for(ObjectInstance mbean : mbeans) {
+                ObjectName objectName = mbean.getObjectName();
+                System.out.println("MBean Name: " + objectName.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
