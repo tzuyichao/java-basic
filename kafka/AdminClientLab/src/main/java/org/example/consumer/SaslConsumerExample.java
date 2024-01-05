101c1f2f2f13d4437725d1bed2da3bd06d422956
TERENCE.CHAO <terence.chao@deltaww.com>
2024-01-05 16:00:48 +0800

    simple consumer lab

---
+++ kafka/AdminClientLab/src/main/java/org/example/consumer/SaslConsumerExample.java
@@ -0,0 +1,55 @@
+package org.example.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class SaslConsumerExample {
+    private static final Logger logger = LoggerFactory.getLogger(SaslConsumerExample.class);
+    public static void main(String[] args) {
+        String bootstrapServers = "datagovstg-kfk01.deltaww.com:9093";
+        String topic = "product.project-management.project.project-change.v0-dev";
+        String groupId = "NPE-Kafka-Account";
+
+        // Kafka consumer configuration settings
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // SASL configuration
+        properties.setProperty("security.protocol", "SASL_PLAINTEXT");
+        properties.setProperty("sasl.mechanism", "SCRAM-SHA-512");
+        properties.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username='NPE-Kafka-Account' password='passwpord';");
+
+        // Create the consumer
+        try(KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);) {
+
+            // Subscribe to the topic
+            consumer.subscribe(Collections.singletonList(topic));
+
+            System.out.println(consumer.groupMetadata());
+
+            // Poll for new data
+            while (true) {
+                ConsumerRecords<String, String> records =
+                        consumer.poll(Duration.ofMillis(100));
+
+                for (ConsumerRecord<String, String> record : records){
+                    logger.info("Key: " + record.key() + ", Value: " + record.value());
+                    logger.info("Partition: " + record.partition() + ", Offset:" + record.offset());
+                }
+            }
+        }
+    }
+}
