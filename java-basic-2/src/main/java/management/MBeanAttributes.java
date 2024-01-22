e8a371beb3c4d3d9abe0a3efb0c9e1e8ac9969ea
TERENCE.CHAO <terence.chao@deltaww.com>
2024-01-22 14:18:13 +0800

    attribute lab

--- java-basic-2/src/main/java/management/MBeanAttributes.java
+++ java-basic-2/src/main/java/management/MBeanAttributes.java
@@ -9,14 +9,15 @@ import java.util.Set;
 public class MBeanAttributes {
     public static void main(String[] args) {
         try {
-            JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://datagovstg-kfk01.deltaww.com:9996/jmxrmi");
+            JMXServiceURL url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://datagov-kfk01.deltaww.com:9996/jmxrmi");
             JMXConnector jmxc = JMXConnectorFactory.connect(url, null);
             MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
             Set<ObjectInstance> mbeans = mbsc.queryMBeans(null, null);
             for(ObjectInstance mbean : mbeans) {
                 ObjectName objectName = mbean.getObjectName();
-                System.out.println("MBean Name: " + objectName.toString());
-                if("debezium.sql_server:type=connector-metrics,server=datagov.pipeline.mgt.syncerp_qas_db,task=0,context=streaming,database=SYNCERP".equals(objectName.toString())) {
+                //System.out.println("MBean Name: " + objectName.toString());
+                if(objectName.toString().startsWith("debezium")) {
+                    System.out.println("MBean Name: " + objectName.toString());
                     MBeanInfo mBeanInfo = mbsc.getMBeanInfo(objectName);
                     MBeanAttributeInfo[] attrInfos = mBeanInfo.getAttributes();
                     for(MBeanAttributeInfo attrInfo: attrInfos) {
@@ -28,5 +29,6 @@ public class MBeanAttributes {
             jmxc.close();
         } catch (Exception e) {
             e.printStackTrace();
-        }    }
+        }
+    }
 }
