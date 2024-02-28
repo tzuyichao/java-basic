3a7d185e81f3b15d84140016cfdbc023756800a3
TzuYi Chao <tzuyichao0313@gmail.com>
2024-02-28 10:57:30 +0800

    set retention.ms to desire

---
+++ kafka/AdminClientLab/src/main/java/org/example/topic/RetentionConfig.java
@@ -0,0 +1,40 @@
+package org.example.topic;
+
+import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.common.config.ConfigResource;
+import org.apache.kafka.common.config.TopicConfig;
+
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+
+public class RetentionConfig {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
+
+        try (AdminClient adminClient = AdminClient.create(props)) {
+            var topics = adminClient.listTopics().names().get();
+
+            for (String topic : topics) {
+                ConfigResource resource = new ConfigResource(ConfigResource.Type.TOPIC, topic);
+                DescribeConfigsResult describeConfigsResult = adminClient.describeConfigs(Collections.singletonList(resource));
+                Config config = describeConfigsResult.all().get().get(resource);
+
+                String retentionStr = config.get(TopicConfig.RETENTION_MS_CONFIG).value();
+                long currentRetention = retentionStr != null ? Long.parseLong(retentionStr) : 0;
+
+                if (currentRetention < 604800000) {
+                    Map<ConfigResource, Collection<AlterConfigOp>> updateConfigs = new HashMap<>();
+                    ConfigEntry retentionEntry = new ConfigEntry(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(604800000));
+                    AlterConfigOp op = new AlterConfigOp(retentionEntry, AlterConfigOp.OpType.SET);
+                    updateConfigs.put(resource, Collections.singletonList(op));
+
+                    adminClient.incrementalAlterConfigs(updateConfigs).all().get();
+                    System.out.println("Updated retention for topic: " + topic);
+                }
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
