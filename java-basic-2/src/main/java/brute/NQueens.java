dccbdbae023185a35e27b563245409bd3a4a0eef
TzuYi Chao <tzuyichao0313@gmail.com>
2024-07-20 11:07:03 +0800

    Update NQueens.java

--- java-basic-2/src/main/java/brute/NQueens.java
+++ java-basic-2/src/main/java/brute/NQueens.java
@@ -1,33 +1,67 @@
 package brute;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-/**
- * 51. N-Queens
- * https://leetcode.com/problems/n-queens/
- */
 public class NQueens {
-    private boolean checkQueue(int x, int y, List<StringBuilder> table, int n) {
-        for(var i=0; i<y; i++) {
-            if(table.get(i).charAt(x) == 'Q') {
-                return false;
+    public static void main(String[] args) {
+        int n = 8;
+        List<List<String>> solutions = solveNQueens(n);
+        for (List<String> solution : solutions) {
+            for (String row : solution) {
+                System.out.println(row);
+            }
+            System.out.println();
+        }
+    }
+
+    public static List<List<String>> solveNQueens(int n) {
+        List<List<String>> solutions = new ArrayList<>();
+        char[] emptyRow = new char[n];
+        Arrays.fill(emptyRow, '.');
+        solve(solutions, new ArrayList<>(), emptyRow, n);
+        return solutions;
+    }
+
+    private static void solve(List<List<String>> solutions, List<StringBuilder> board, char[] emptyRow, int n) {
+        if (board.size() == n) {
+            List<String> solution = new ArrayList<>();
+            for (StringBuilder row : board) {
+                solution.add(row.toString());
+            }
+            solutions.add(solution);
+            return;
+        }
+
+        int row = board.size();
+        for (int col = 0; col < n; col++) {
+            if (isValid(board, row, col, n)) {
+                StringBuilder newRow = new StringBuilder(new String(emptyRow));
+                newRow.setCharAt(col, 'Q');
+                board.add(newRow);
+                solve(solutions, board, emptyRow, n);
+                board.removeLast();
             }
         }
-        for(int i=x-1, j=y-1; i>=0&&j>=0; i--, j--) {
-            if(table.get(j).charAt(i) == 'Q') {
+    }
+
+    private static boolean isValid(List<StringBuilder> board, int row, int col, int n) {
+        for (int i = 0; i < row; i++) {
+            StringBuilder currentRow = board.get(i);
+            if (currentRow.charAt(col) == 'Q') {
                 return false;
             }
-        }
-        for(int i=x+1, j=y-1; i<n&&j>=0; j--, i++) {
-            if(table.get(j).charAt(i) == 'Q') {
+            int leftDiagonal = col - (row - i);
+            int rightDiagonal = col + (row - i);
+            if (leftDiagonal >= 0 && currentRow.charAt(leftDiagonal) == 'Q') {
+                return false;
+            }
+            if (rightDiagonal < n && currentRow.charAt(rightDiagonal) == 'Q') {
                 return false;
             }
         }
         return true;
     }
-
-    public List<List<String>> solveNQueens(int n) {
-        return Collections.EMPTY_LIST;
-    }
 }
+
