da965a6d7698dd0425a652aa1f79fe45e5eab120
TzuYi Chao <tzuyichao0313@gmail.com>
2024-07-24 11:35:21 +0800

    慢

--- java-basic-2/src/main/java/brute/SortTheJumbledNumbers.java
+++ java-basic-2/src/main/java/brute/SortTheJumbledNumbers.java
@@ -1,43 +1,43 @@
 package brute;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class SortTheJumbledNumbers {
     public int[] sortJumbled(int[] mapping, int[] nums) {
-        int[] mapped = Arrays.stream(nums).map(val -> {
+        List<int[]> mapped = new ArrayList<>();
+
+        IntStream.range(0, nums.length).forEach(val -> {
+            int[] pair = new int[2];
+            int original = nums[val];
             int result = 0;
             int multiplier = 1;
-            if(val == 0) {
-                return mapping[0];
+            if(original == 0) {
+                pair[0] = mapping[original];;
+                pair[1] = val;
+                mapped.add(pair);
+            } else {
+                while (original > 0) {
+                    int digit = original % 10;
+                    int mappedDigit = mapping[digit];
+                    result += mappedDigit * multiplier;
+                    multiplier *= 10;
+                    original /= 10;
+                }
+                pair[0] = result;
+                pair[1] = val;
+                mapped.add(pair);
             }
-            while (val > 0) {
-                int digit = val % 10;
-                int mappedDigit = mapping[digit];
-                result += mappedDigit * multiplier;
-                multiplier *= 10;
-                val /= 10;
-            }
-            return result;
-        }).toArray();
+        });
 
-        for (int n : mapped) {
-            System.out.print(n + " ");
+        mapped.forEach(pair -> System.out.println(pair[0] + " " + pair[1]));
+
+        Collections.sort(mapped, Comparator.comparingInt(a -> a[0]));
+
+        int[] ans = new int[nums.length];
+        for(int i = 0; i < nums.length; i++) {
+            ans[i] = nums[mapped.get(i)[1]];
         }
-
-        int len = mapped.length;
-        for(int i = 1; i < len; i++) {
-            int val = mapped[i];
-            int nval = nums[i];
-            int j = i - 1;
-
-            while(j >= 0 && val < mapped[j]) {
-                mapped[j+1] = mapped[j];
-                nums[j+1] = nums[j];
-                j--;
-            }
-            mapped[j+1] = val;
-            nums[j+1] = nval;
-        }
-        return nums;
+        return ans;
     }
 }
