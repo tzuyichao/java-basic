d8049ae56e9cbaca9aef432316848f83fbaebf88
TERENCE.CHAO <terence.chao@deltaww.com>
2024-08-22 13:06:11 +0800

    for lab

--- kafka/AdminClientLab/src/main/java/org/example/producer/ProducerWithAckLab.java
+++ kafka/AdminClientLab/src/main/java/org/example/producer/ProducerWithAckLab.java
@@ -19,33 +19,28 @@ public class ProducerWithAckLab {
         props.put("sasl.mechanism", dotenv.get("SASL_MECHANISM"));
         props.put("auto.offset.reset","earliest");
 
-        //props.put("sasl.jaas.config", dotenv.get("JAAS"));
-        String jaas = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"PFMEA-DEV-Kafka-Account\" password=\"111\";";
-        props.put("sasl.jaas.config", jaas);
+        props.put("sasl.jaas.config", dotenv.get("JAAS"));
+        //String jaas = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"PFMEA-DEV-Kafka-Account\" password=\"111\";";
+        //props.put("sasl.jaas.config", jaas);
 
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         // acks = all => all replicas
         props.put("acks", "all");
-        Producer<String, String> producer = new KafkaProducer<>(props);
+        try(Producer<String, String> producer = new KafkaProducer<>(props);) {
+            String topic = "test.testopic.v0";
+            String key = "my-key";
+            String value = "Hello, Kafka!";
 
-        String topic = "test.testopic.v0";
-        String key = "my-key";
-        String value = "Hello, Kafka!";
-
-        ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
-        producer.send(record, new Callback() {
-            @Override
-            public void onCompletion(RecordMetadata metadata, Exception exception) {
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+            producer.send(record, (metadata, exception) -> {
                 if (exception == null) {
                     System.out.println("Message sent successfully. Offset: " + metadata.offset());
                 } else {
                     System.out.println("Failed to send message: " + exception.getMessage());
                 }
-            }
-        });
-
-        producer.close();
+            });
+        }
     }
 }
