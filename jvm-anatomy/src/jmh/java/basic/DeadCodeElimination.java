8a32354d03eb9bea4a3f90876e9692fe51316bc7
TzuYi Chao <tzuyichao0313@gmail.com>
2025-04-13 22:48:13 +0800

    test

---
+++ jvm-anatomy/src/jmh/java/basic/DeadCodeElimination.java
@@ -0,0 +1,44 @@
+package basic;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Source: https://www.baeldung.com/java-microbenchmark-harness
+ */
+@Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 1)
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class DeadCodeElimination {
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @BenchmarkMode(Mode.AverageTime)
+    public void doNothing() {
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @BenchmarkMode(Mode.AverageTime)
+    public void objectCreation() {
+        new Object();
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @BenchmarkMode(Mode.AverageTime)
+    public Object pillarsOfCreation() {
+        return new Object();
+    }
+
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @BenchmarkMode(Mode.AverageTime)
+    public void blackHole(Blackhole blackhole) {
+        blackhole.consume(new Object());
+    }
+}
