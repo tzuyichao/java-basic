9102b9030dc6d4c61bb9d1b004216670274fc2c7
TzuYi Chao <tzuyichao0313@gmail.com>
2025-08-16 09:55:19 +0800

    Update PreparedStatementBingTypeLab.java

--- mssql-jdbc-lab/src/main/java/type/PreparedStatementBingTypeLab.java
+++ mssql-jdbc-lab/src/main/java/type/PreparedStatementBingTypeLab.java
@@ -33,8 +33,20 @@ public class PreparedStatementBingTypeLab {
         }
     }
 
+    public void updateForConvertImplicit2() {
+        final String sql = "Update convert_implicit_test2 SET Name=? WHERE DeptCode=?";
+        try(Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
+            preparedStatement.setNString(1, "DEPT2");
+            preparedStatement.setNString(2, "0002");
+            preparedStatement.executeUpdate();
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         PreparedStatementBingTypeLab preparedStatementBingTypeLab = new PreparedStatementBingTypeLab();
-        preparedStatementBingTypeLab.updateForConvertImplicit();
+        preparedStatementBingTypeLab.updateForConvertImplicit2();
     }
 }
