a8a0ead6cf66921379243cdcb8ec355df96fa379
TzuYi Chao <tzuyichao0313@gmail.com>
2025-10-05 16:15:00 +0800

    first add

---
+++ kafka/connect/lowercase-topic/src/main/java/demo/LowercaseTopic.java
@@ -0,0 +1,45 @@
+package demo;
+
+import org.apache.kafka.common.Configurable;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.transforms.Transformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Locale;
+import java.util.Map;
+
+public class LowercaseTopic<R extends ConnectRecord<R>> implements Transformation<R>, Configurable {
+    private static final Logger log = LoggerFactory.getLogger(LowercaseTopic.class);
+
+    @Override
+    public R apply(R record) {
+        if (record == null || record.topic() == null) return record;
+        String lower = record.topic().toLowerCase(Locale.ROOT);
+        if (lower.equals(record.topic())) return record;
+        log.info("lowercased from {} to {}", record.topic(), lower);
+        return record.newRecord(
+                lower,
+                record.kafkaPartition(),
+                record.keySchema(), record.key(),
+                record.valueSchema(), record.value(),
+                record.timestamp()
+        );
+    }
+
+    @Override
+    public ConfigDef config() {
+        return new ConfigDef();
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+    }
+}
